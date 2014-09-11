14c637c417f19a6df4f6b42122d70d0dcb765240
Server <you@example.com>
2014-09-11 11:11:00 +0200

    added new test behavior goto position with ball

---
+++ server/src/robocup/controller/ai/highLevelBehavior/testbehavior/TestPositionWithBallBehavior.java
@@ -0,0 +1,106 @@
+package robocup.controller.ai.highLevelBehavior.testbehavior;
+
+import java.util.ArrayList;
+
+import robocup.controller.ai.highLevelBehavior.Behavior;
+import robocup.controller.ai.lowLevelBehavior.GotoPosition;
+import robocup.controller.ai.lowLevelBehavior.RobotExecuter;
+import robocup.model.Ball;
+import robocup.model.Point;
+import robocup.model.Robot;
+import robocup.model.World;
+import robocup.output.ComInterface;
+import robocup.output.RobotCom;
+
+public class TestPositionWithBallBehavior extends Behavior {
+
+	ArrayList<Robot> robots = new ArrayList<>();
+
+	@Override
+	public void execute(ArrayList<RobotExecuter> executers) {
+		World w = World.getInstance();
+		robots = w.getAlly().getRobots();
+		Robot closest = getClosestToTarget(robots, World.getInstance().getBall().getPosition());
+		for( Robot r  : robots){
+			try {
+				RobotExecuter executer = findExecuter(r.getRobotID(), executers);
+				if( executer == null ) {
+					executer = new RobotExecuter(r);
+					executer.setLowLevelBehavior(new GotoPosition(r, ComInterface.getInstance(RobotCom.class), w.getBall().getPosition()));
+					new Thread(executer).start();
+					executers.add(executer);
+				}
+				if(w.getBall().getPosition()!= null && r.getPosition().getDeltaDistance(w.getBall().getPosition() )> 700)
+					goToBall(r, executer);
+				else if(r.equals(closest)){
+					goToBall(r, executer);
+				} else {
+					((GotoPosition) executer.getLowLevelBehavior()).setTarget(null);
+				}
+			}catch(NullPointerException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void goToBall(Robot r, RobotExecuter e){
+		
+		Ball b = World.getInstance().getBall();
+		
+		System.out.println(" Bal pos: " +  b.getPosition());
+	
+		GotoPosition go = null;
+		if(e.getLowLevelBehavior() instanceof GotoPosition) {
+			go = (GotoPosition)e.getLowLevelBehavior();
+		}
+		
+		
+		// check if new position is within the worlds bounds
+		//System.out.println(b.getPosition());
+		//System.out.println(World.getInstance().getField().getLength() + "  " + World.getInstance().getField().getWidth());
+		
+		//>prutteltje testing temp
+		Point p = null;
+		// Get field params
+		float fieldX = World.getInstance().getField().getLength() / 2;
+		float fieldY = World.getInstance().getField().getWidth() / 2;
+		float targetPositionX = b.getPosition().getX();
+		float targetPositionY = b.getPosition().getY();
+		float borderZoneX = 100f;
+		float borderZoneY = 100f;
+		
+		if(Math.abs(targetPositionX) < (fieldX - borderZoneX) && Math.abs(targetPositionY) < (fieldY - borderZoneY)) {
+			p = new Point(targetPositionX, targetPositionY);
+			
+			go.setTarget(p);
+		}
+		else{
+			go.setTarget(null);
+		}
+		//go.setTarget(b.getPosition());
+	}
+	
+	private Robot getClosestToTarget(ArrayList<Robot> robots, Point p){
+		Robot closest = null;
+		double distance = Double.MAX_VALUE;
+		for( Robot r : robots){
+			Point robotPosition = r.getPosition();
+//			System.out.println(r.isOnSight());
+			if(robotPosition != null && p != null && r.isOnSight()){
+				if( r.getPosition().getDeltaDistance(p) < distance ){
+					closest = r;
+					distance = r.getPosition().getDeltaDistance(p);
+				}
+			}
+		}
+		return closest;
+	}
+	
+	public RobotExecuter findExecuter(int robotId, ArrayList<RobotExecuter> executers){
+		for(RobotExecuter r : executers){
+			if(r.getRobot().getRobotID() == robotId)
+				return r;
+		}
+		return null;
+	}
+}
--- server/src/robocup/controller/ai/lowLevelBehavior/GotoPositionWithBall.java
+++ server/src/robocup/controller/ai/lowLevelBehavior/GotoPositionWithBall.java
@@ -5,22 +5,26 @@ import robocup.model.Point;
 import robocup.model.Robot;
 import robocup.output.ComInterface;
 
-
-//Robot receives location of ball
+//Robot receives ball location and target position
+//robot drives to ball and starts dribbler.
+//robot drives to target location with the ball.
 public class GotoPositionWithBall extends LowLevelBehavior {
 
 	private Point newPosition = null;
+	private Point ballPosition = null;
 
-	public GotoPositionWithBall(Robot robot, ComInterface output, Point NewPosition) {
+	public GotoPositionWithBall(Robot robot, ComInterface output, Point targetPosition, Point BallPosition) {
 		super(robot, output);
-		this.newPosition = NewPosition;
+		this.newPosition = targetPosition;
+		this.ballPosition = BallPosition;
 		// TODO Auto-generated constructor stub
 	}
 
-	public GotoPositionWithBall(Robot robot, ComInterface output, FieldObject target) {
-		super(robot, output);
-		this.newPosition = target.getPosition();
-	}
+	// public GotoPositionWithBall(Robot robot, ComInterface output, FieldObject
+	// target) {
+	// super(robot, output);
+	// this.newPosition = target.getPosition();
+	// }
 
 	public Point getTarget() {
 		return newPosition;
@@ -29,90 +33,144 @@ public class GotoPositionWithBall extends LowLevelBehavior {
 	public void setTarget(Point p) {
 		this.newPosition = p;
 	}
-	
-	
+
+	// zolang de ball niet binnen een bepaalde afstand van de robot ligt moet
+	// hij naar de bal rijden.
+	// zodra de ball in de buurt van de robot ligt moet hij dribbler aanzetten
+	// en deze 'oppakken'
+	// met de ball moet de robot naar de aangegeven plek rijden.
 
 	@Override
 	public void calculate() {
-		if(timeOutCheck()){
-				
-		}else if(newPosition == null){
-			robot.setOnSight(true);
-			output.send(1,  robot.getRobotID(), 0, 0, 0, 0, 0, 0, false);
-			return;
-		}else{
-			robot.setOnSight(true);
-			int direction = 0;
-			int travelDistance = 0;
-			int newRotation = rotationToDest(newPosition);
-			int speed = 0;
-			int rotationSpeed = 0;
-			
-			if(Math.abs(newRotation) > 140){
-				rotationSpeed = 350;
-			}else if(Math.abs(newRotation) > 70) {
-				rotationSpeed = 280;
-			}else if(Math.abs(newRotation) > 30) {
-				travelDistance = getDistance();
-				speed = getSpeed(travelDistance, newRotation);
-				if(speed == 0)
-					rotationSpeed = 400;
-				else
-					rotationSpeed = 300;
-			} else if(Math.abs(newRotation) > 10) {
-				travelDistance = getDistance();
-				speed = getSpeed(travelDistance, newRotation);
-				if(speed == 0)
-					rotationSpeed = 200;
-				else
-					rotationSpeed = 150;
-			} else {
-				travelDistance = getDistance();
-				speed = getSpeed(travelDistance, newRotation);
-			}
-			
-			if(newRotation < 0)
-				rotationSpeed *= -1;
-			if(travelDistance < 300 && Math.abs(newRotation) > 10)
-				speed = 0;
-			output.send(1, robot.getRobotID(), direction, speed, travelDistance, newRotation, rotationSpeed, 0, false);
-		}
-	}
-	
-	public int getDistance(){
-		double dx = (robot.getPosition().getX() - newPosition.getX());
-		double dy = (robot.getPosition().getY() - newPosition.getY());
-		return (int)Math.sqrt(dx*dx + dy*dy);
-	}
-	
-	public int getSpeed(int distance, int rotationToDest){
-		int speed = 7000;
-		if( (int)(distance/1.6) < speed){
-			speed = (int)(distance/1.0);
-		}
-		if( Math.abs(rotationToDest) > 10 ){
-			speed = 900;
-				if( (int)(distance/2) < speed)
-					speed = (int)(distance/2);
-		}
-//		if(Math.abs(rotationToDest) >= 1 ){
-//			speed -= (int)((float)speed / ((float)90 / (float)Math.abs(rotationToDest)));
-//		}
-		if(speed < 400)
-			speed = 400;
-//		if(Math.abs(rotationToDest) > 30){
-//			speed = 0;
-//		}
-//		if(Math.abs(rotationToDest) > 10 && distance < 300)
-//			speed = 0;
+		if (timeOutCheck()) {
 
-//		
-//		if( Math.abs(rotationToDest) > 10 ){
-////			speed = 400;
-//			speed /= (Math.abs(rotationToDest)/10);
-//			if( (int)(distance/2) < speed)
-//				speed = (int)(distance/2);
-//		}
+		} else if (ballPosition == null) {
+			robot.setOnSight(true);
+			output.send(1, robot.getRobotID(), 0, 0, 0, 0, 0, 0, false);
+			return;
+		} else {
+			if (newPosition.getX() - ballPosition.getX() > 10 && newPosition.getY() - ballPosition.getY() > 10) {
+				robot.setOnSight(true);
+				int direction = 0;
+				int travelDistance = 0;
+				int newRotation = rotationToDest(ballPosition);
+				int speed = 0;
+				int rotationSpeed = 0;
+
+				if (Math.abs(newRotation) > 140) {
+					rotationSpeed = 350;
+				} else if (Math.abs(newRotation) > 70) {
+					rotationSpeed = 280;
+				} else if (Math.abs(newRotation) > 30) {
+					travelDistance = getDistanceToTarget(ballPosition);
+					speed = getSpeed(travelDistance, newRotation);
+					if (speed == 0)
+						rotationSpeed = 400;
+					else
+						rotationSpeed = 300;
+				} else if (Math.abs(newRotation) > 10) {
+					travelDistance = getDistanceToTarget(ballPosition);
+					speed = getSpeed(travelDistance, newRotation);
+					if (speed == 0)
+						rotationSpeed = 200;
+					else
+						rotationSpeed = 150;
+				} else {
+					travelDistance = getDistanceToTarget(ballPosition);
+					speed = getSpeed(travelDistance, newRotation);
+				}
+
+				if (newRotation < 0)
+					rotationSpeed *= -1;
+				if (travelDistance < 300 && Math.abs(newRotation) > 10)
+					speed = 0;
+				output.send(1, robot.getRobotID(), direction, speed, travelDistance, newRotation, rotationSpeed, 0,
+						false);
+			}
+
+			else {
+
+				robot.setOnSight(true);
+				int direction = 0;
+				int travelDistance = 0;
+				int newRotation = rotationToDest(newPosition);
+				int speed = 0;
+				int rotationSpeed = 0;
+
+				if (Math.abs(newRotation) > 140) {
+					rotationSpeed = 350;
+				} else if (Math.abs(newRotation) > 70) {
+					rotationSpeed = 280;
+				} else if (Math.abs(newRotation) > 30) {
+					travelDistance = getDistanceToTarget(newPosition);
+					speed = getSpeed(travelDistance, newRotation);
+					if (speed == 0)
+						rotationSpeed = 400;
+					else
+						rotationSpeed = 300;
+				} else if (Math.abs(newRotation) > 10) {
+					travelDistance = getDistanceToTarget(newPosition);
+					speed = getSpeed(travelDistance, newRotation);
+					if (speed == 0)
+						rotationSpeed = 200;
+					else
+						rotationSpeed = 150;
+				} else {
+					travelDistance = getDistanceToTarget(newPosition);
+					speed = getSpeed(travelDistance, newRotation);
+				}
+
+				if (newRotation < 0)
+					rotationSpeed *= -1;
+				if (travelDistance < 300 && Math.abs(newRotation) > 10)
+					speed = 0;
+				output.send(1, robot.getRobotID(), direction, speed, travelDistance, newRotation, rotationSpeed, 0,
+						true);
+			}
+		}
+	}
+
+	// public int getDistanceToTarget() {
+	// double dx = (robot.getPosition().getX() - newPosition.getX());
+	// double dy = (robot.getPosition().getY() - newPosition.getY());
+	// return (int) Math.sqrt(dx * dx + dy * dy);
+	// }
+
+	public int getDistanceToTarget(Point target) {
+		double dx = (robot.getPosition().getX() - target.getX());
+		double dy = (robot.getPosition().getY() - target.getY());
+		return (int) Math.sqrt(dx * dx + dy * dy);
+	}
+
+	public int getSpeed(int distance, int rotationToDest) {
+		int speed = 7000;
+		if ((int) (distance / 1.6) < speed) {
+			speed = (int) (distance / 1.0);
+		}
+		if (Math.abs(rotationToDest) > 10) {
+			speed = 900;
+			if ((int) (distance / 2) < speed)
+				speed = (int) (distance / 2);
+		}
+		// if(Math.abs(rotationToDest) >= 1 ){
+		// speed -= (int)((float)speed / ((float)90 /
+		// (float)Math.abs(rotationToDest)));
+		// }
+		if (speed < 400)
+			speed = 400;
+		// if(Math.abs(rotationToDest) > 30){
+		// speed = 0;
+		// }
+		// if(Math.abs(rotationToDest) > 10 && distance < 300)
+		// speed = 0;
+
+		//
+		// if( Math.abs(rotationToDest) > 10 ){
+		// // speed = 400;
+		// speed /= (Math.abs(rotationToDest)/10);
+		// if( (int)(distance/2) < speed)
+		// speed = (int)(distance/2);
+		// }
 
 		return speed;
 	}
