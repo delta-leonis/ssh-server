f22ca257bfc9ac35f5a8efa6bcd5c803cab714cf
CrashO <rob@tikgeit.eu>
2014-10-01 10:20:46 +0200

    Initiele test van de Fuckrobot

---
+++ server/src/robocup/controller/ai/highLevelBehavior/testbehavior/TestFuckRobotBehavior.java
@@ -0,0 +1,55 @@
+package robocup.controller.ai.highLevelBehavior.testbehavior;
+
+import java.util.ArrayList;
+
+import robocup.Main;
+import robocup.controller.ai.highLevelBehavior.Behavior;
+import robocup.controller.ai.lowLevelBehavior.FuckRobot;
+import robocup.controller.ai.lowLevelBehavior.RobotExecuter;
+import robocup.model.Ball;
+import robocup.model.FieldObject;
+import robocup.model.Point;
+import robocup.model.Robot;
+import robocup.model.World;
+import robocup.output.ComInterface;
+import robocup.output.RobotCom;
+
+public class TestFuckRobotBehavior extends Behavior {
+	
+
+	
+	private Ball ball;
+	private World world;
+	private Robot defender;
+	private Robot oponent;
+	
+	public TestFuckRobotBehavior() {
+		world = World.getInstance();
+	}
+	
+	@Override
+	public void execute(ArrayList<RobotExecuter> executers) {
+		defender = world.getAlly().getRobotByID(Main.TEST_ROBOT_ID);
+		oponent = world.getEnemy().getRobotByID(Main.TEST_FUCK_ROBOT_ID);
+		
+		
+		ball = world.getBall();
+		int distanceToOponent = 500;
+		
+		if(defender != null && ball != null) {
+			RobotExecuter executer = findExecuter(Main.TEST_ROBOT_ID, executers);
+			
+			// Initialize executer for this robot
+			if(executer == null) {
+				executer = new RobotExecuter(defender);
+				executer.setLowLevelBehavior(new FuckRobot(defender, ComInterface.getInstance(RobotCom.class), distanceToOponent, false, 
+						ball.getPosition(), defender.getPosition(), oponent.getPosition()));
+				new Thread(executer).start();
+				executers.add(executer);
+			} else {
+				((FuckRobot)executer.getLowLevelBehavior()).update(distanceToOponent, false, ball.getPosition(), defender.getPosition());
+			}
+		}
+	}
+	
+}
--- server/src/robocup/controller/ai/lowLevelBehavior/FuckRobot.java
+++ server/src/robocup/controller/ai/lowLevelBehavior/FuckRobot.java
@@ -1,19 +1,89 @@
 package robocup.controller.ai.lowLevelBehavior;
 
+import robocup.model.Point;
 import robocup.model.Robot;
 import robocup.output.ComInterface;
 
 public class FuckRobot extends LowLevelBehavior {
 
-	public FuckRobot(Robot robot, ComInterface output) {
+	protected Point ballPosition;
+	protected int distanceToOponent;
+	protected Point oponentPosition;
+	protected Point defenderPosition;
+	
+	/**
+	 * Create a defender (stand between "target" enemy and ball)
+	 * @param robot
+	 * @param output
+	 * @param distanceToOponent defense radius size, 40?? ideal in most situations
+	 * @param goToKick if true, move to ball and kick it away
+	 * @param ballPosition current position of the ball
+	 * @param defenderPosition current position of the defender (this)
+	 * @param oponentPosition center position of the oponent
+	 */
+	public FuckRobot(Robot robot, ComInterface output, int distanceToOponent, boolean goToKick, Point ballPosition,
+			Point defenderPosition, Point oponentPosition) {
 		super(robot, output);
-		// TODO Auto-generated constructor stub
+		
+		this.oponentPosition = oponentPosition;
+		this.ballPosition = ballPosition;
+		this.distanceToOponent = distanceToOponent;
+		this.defenderPosition = defenderPosition;
 	}
-
+	
+	
+	public void update(int distanceToOponent, boolean goToKick, Point ballPosition, Point defenderPosition) {
+		this.distanceToOponent = distanceToOponent;
+		//this.goToKick = goToKick;
+		this.ballPosition = ballPosition;
+		this.defenderPosition = defenderPosition;
+	}
+	
 	@Override
 	public void calculate() {
-		// TODO Auto-generated method stub
 		
+		if(timeOutCheck()) {
+			
+		} else {
+			
+			Point newDestination = getNewDestination();
+			
+			if(newDestination != null) {
+				/*if(goToKick)
+					go.setGoal(ballPosition);//GotoPosition(keeperPosition, ballPosition, ballPosition)
+				else
+					go.setGoal(newDestination);//GotoPosition(keeperPosition, newDestination, ballPosition)
+			 	*/
+				go.setTarget(ballPosition);
+				go.calculate();
+			}
+			
+			
+		}
+
 	}
 
+	private Point getNewDestination() {
+		Point newDestination = null;
+		
+		if(ballPosition != null) {
+			
+			int angle = Math.abs(oponentPosition.getAngle(ballPosition));
+			int realAngle = angle > 90 ? 180 - angle : angle;
+			
+			double dx = Math.sin(Math.toRadians(realAngle)) * distanceToOponent;
+			double dy = Math.sqrt(distanceToOponent * distanceToOponent - dx * dx);
+			
+			int oponentX = (int) oponentPosition.getX();
+			int destX = (int) (oponentX > 0 ? oponentX - dx : oponentX + dx);
+			
+			int oponentY = (int) oponentPosition.getY();
+			int destY = (int) (ballPosition.getY() > 0 ? oponentY + dy : oponentY - dy);
+		
+			newDestination = new Point(destX, destY);
+		}
+
+		return newDestination;
+	}
+	
 }
