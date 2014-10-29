ee0ed90d59b241a6b0b8d1bedaf5bf9efbbb4d84
Server <you@example.com>
2014-10-29 15:50:17 +0100

    improved pathplanner

--- server/src/robocup/controller/ai/movement/PathPlanner.java
+++ server/src/robocup/controller/ai/movement/PathPlanner.java
@@ -8,7 +8,6 @@ import robocup.model.Point;
 import robocup.model.Robot;
 import robocup.model.World;
 
-
 public class PathPlanner {
 	ArrayList<Robot> objects;
 
@@ -23,61 +22,78 @@ public class PathPlanner {
 
 	/**
 	 * get the next free node on the route to the endNode
-	 * @param beginNode to calculate the best possible route a beginNode is needed
-	 * @param endNode to calculate the best possible route a endNode is needed
+	 * 
+	 * @param beginNode
+	 *            to calculate the best possible route a beginNode is needed
+	 * @param endNode
+	 *            to calculate the best possible route a endNode is needed
 	 * @return next available free node on the route
 	 */
 	public Point getNextRoutePoint(Point beginNode, Point endNode, int robotId) {
-		Point subNode = endNode;
-		Line2D line = new Line2D.Float(beginNode.getX(), beginNode.getY(), subNode.getX(), subNode.getY());
-		while (true) {
-//			System.out.println("nop");
-//			System.out.println("beginNodeX: "+beginNode.getX() + " beginNodeY: " + beginNode.getY());
-//			System.out.println("subNodeX: "+subNode.getX() + " subNodeY: " + subNode.getY());
-			Rectangle2D temp = lineIntersectsObject(line, robotId);
-//			System.out.println("temp: " + temp);
-			if(temp == null){
-				
-				break;
-			}
-			subNode = getNewSubPoint(temp, beginNode);
+
+		//create new line
+		Line2D line = new Line2D.Float(beginNode.getX(), beginNode.getY(), endNode.getX(), endNode.getY());
+		//get intersecting object(rectangle)
+		Rectangle2D temp = lineIntersectsObject(line, robotId);
+		//init subNode to null
+		Point subNode = null;
+		//while there are intersecting objects keep inside the loop
+		while (temp != null) {
+			//make new subNode
+			subNode = getNewSubPoint(temp, beginNode, subNode);
 			line = new Line2D.Float(beginNode.getX(), beginNode.getY(), subNode.getX(), subNode.getY());
+			temp = lineIntersectsObject(line, robotId);
+			System.out.println(subNode);
 
 		}
-//		System.out.println("snx:" + subNode.getX() + " snY: " + subNode.getY());
-		return subNode;
+		if (subNode != null) {
+			System.out.println("returned subNode");
+			return subNode;
+		} else {
+			System.out.println("returned endNode");
+			return endNode;
+		}
 	}
+
 	/**
-	 * Check if one of the robots intersects the line on which the robot is going to travel
-	 * @param line A Line2D is needed to check if there is an intersection with a robot object
-	 * @return return the object 
+	 * Check if one of the robots intersects the line on which the robot is
+	 * going to travel
+	 * 
+	 * @param lineline
+	 *            A Line2D is needed to check if there is an intersection with a
+	 *            robot object
+	 * @return return the object
 	 */
-	private Rectangle2D lineIntersectsObject(Line2D line, int robotId)
-	{
+	private Rectangle2D lineIntersectsObject(Line2D line, int robotId) {
 		Rectangle2D rect = null;
-		for(Robot r : objects){
-			rect = new Rectangle2D.Float(r.getPosition().getX(), r.getPosition().getY(), 100, 100);
-			if(line.intersects(rect) && r.getRobotID() != robotId){
-//				System.out.println(r.getRobotID());
-//				System.out.println("robot: "+r.getRobotID() + "posx:" + rect.getCenterX() + "posy: " + rect.getCenterY());
+		for (Robot r : objects) {
+			rect = new Rectangle2D.Float(r.getPosition().getX(), r.getPosition().getY(), 300, 300);
+			if (line.intersects(rect) && r.getRobotID() != robotId) {
 				break;
 			}
 			rect = null;
 		}
 		return rect;
 	}
-	
-	private Point getNewSubPoint(Rectangle2D object, Point beginNode){
-		double objectX = object.getCenterX();
-		double objectY = object.getCenterY();
-		double beginX = beginNode.getX();
-		double beginY = beginNode.getY();
-		if(beginX > objectX && beginY > objectY) return new Point((float)objectX - 200, (float)objectY + 200);
-		if(beginX > objectX && beginY < objectY) return new Point((float)objectX + 200, (float)objectY + 200);
-		if(beginX < objectX && beginY > objectY) return new Point((float)objectX + 200, (float)objectY + 200);
-		if(beginX < objectX && beginY < objectY) return new Point((float)objectX - 80, (float)objectY + 80);
-		//get point around intersecting object
-//		System.out.println("returned null from subPoint");
-		return null;
+
+	private Point getNewSubPoint(Rectangle2D object, Point beginNode, Point subNode) {
+		// get new random Point away from obstacle
+		
+		//dx = sin(gamma) * z
+		//dy = cos(gamma) * z
+		//gamma = Math.atan2(target.getX() - x, target.getY() - y)
+
+		if (subNode != null) {
+			
+			
+			
+			//implement method to calculate new points
+			subNode = new Point(subNode.getX() + 200, subNode.getY() );
+		} else {
+			subNode = new Point((float) object.getX(), (float) object.getY());
+		}
+
+		return subNode;
+
 	}
 }
