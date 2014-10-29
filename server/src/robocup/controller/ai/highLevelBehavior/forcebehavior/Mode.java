3519d427859fa7202bea1ce8a3437dde93c9c5fa
Thijs Emmerink <thijs.emmerink@gmail.com>
2014-10-29 11:10:21 +0100

    Javadoc for Mode.java

--- server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/Mode.java
+++ server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/Mode.java
@@ -5,7 +5,6 @@ package robocup.controller.ai.highLevelBehavior.forcebehavior;
 
 import java.util.ArrayList;
 
-import robocup.controller.ai.lowLevelBehavior.LowLevelBehavior;
 import robocup.controller.ai.lowLevelBehavior.RobotExecuter;
 
 public abstract class Mode {
@@ -16,11 +15,29 @@ public abstract class Mode {
 	@SuppressWarnings("unused")
 	private RobotExecuter[] robotExcecuter;
 
+	/**
+	 * Let the calculator recalculate all forces
+	 */
 	public abstract void setFieldForce();
 
+	/**
+	 * Let force calculator determine forces
+	 * then update or generate low level behaviors
+	 * @param executers
+	 */
 	public abstract void execute(ArrayList<RobotExecuter> executers);
 	
+	/**
+	 * Update the values of the low level behavior from the executer
+	 * @param executer the executer
+	 * @param type type of the low level behavior
+	 */
 	public abstract void updateLowLevelBehavior(RobotExecuter executer, String type);
 	
+	/**
+	 * Generate a new low level behavior for this executer
+	 * @param executer the executer
+	 * @param type type of the low level behavior
+	 */
 	public abstract void generateLowLevelBehavior(RobotExecuter executer, String type);
 }
