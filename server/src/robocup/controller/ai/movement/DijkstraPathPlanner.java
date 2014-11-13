b42f87f776105693c421606b14832597b76750c1
Thijs Emmerink <thijs.emmerink@gmail.com>
2014-11-13 15:37:09 +0100

    Javadoc for pathplanner

--- server/src/robocup/controller/ai/movement/DijkstraPathPlanner.java
+++ server/src/robocup/controller/ai/movement/DijkstraPathPlanner.java
@@ -16,6 +16,9 @@ public class DijkstraPathPlanner {
 	private ArrayList<Vertex> vertices;
 	private ArrayList<Vertex> filteredVertices;
 
+	/**
+	 * Create the pathplanner
+	 */
 	public DijkstraPathPlanner() {
 		world = World.getInstance();
 		objects = new ArrayList<Rectangle2D>();
@@ -48,12 +51,20 @@ public class DijkstraPathPlanner {
 	    }
 	}
 	
+	/**
+	 * Get the shortest route from a begin point to a destination for one specific robot
+	 * @param beginNode starting point for the robot
+	 * @param destination destination of the robot
+	 * @param robotId robot id of the robot
+	 * @return list with points forming the shortest route
+	 */
 	public LinkedList<Point> getRoute(Point beginNode, Point destination, int robotId) {
 		LinkedList<Point> route = new LinkedList<Point>();
 		
 		// no object on route
 		if(!intersectsObject(new Vertex(beginNode), new Vertex(destination))) {
 			route.push(destination);
+			return route;
 		}
 
 		generateObjectList(robotId);
@@ -94,6 +105,11 @@ public class DijkstraPathPlanner {
 		return route;
 	}
 
+	/**
+	 * Get the closest neighbour for the vertex u
+	 * @param u the vertex
+	 * @return closest neighbour vertex
+	 */
 	private Vertex getMinDistNeighbour(Vertex u) {
 		int minDist = Integer.MAX_VALUE;
 		Vertex closest = null;
@@ -109,6 +125,10 @@ public class DijkstraPathPlanner {
 		return closest;
 	}
 
+	/**
+	 * Create a rectangle around every robot so we can calculate intersections
+	 * @param robotId the robot id of the robot who needs a path, no rectangle will be created for this robot
+	 */
 	private void generateObjectList(int robotId) {
 		for(Robot r : world.getAlly().getRobots())
 			if(r.getRobotID() != robotId)
@@ -118,6 +138,9 @@ public class DijkstraPathPlanner {
 			objects.add(new Rectangle2D.Float(r.getPosition().getX(), r.getPosition().getY(), 399, 399));
 	}
 
+	/**
+	 * Generate vertices around every robot in the robot list
+	 */
 	private void generateVertices() {
 		for(Rectangle2D rect : objects) {
 			int x = (int) rect.getCenterX();
@@ -130,6 +153,9 @@ public class DijkstraPathPlanner {
 		}
 	}
 
+	/**
+	 * Remove all vertices located inside rectangles
+	 */
 	private void removeCollidingVertices() {
 		for(Rectangle2D rect : objects)
 			for(Vertex v : vertices)
@@ -139,6 +165,9 @@ public class DijkstraPathPlanner {
 		vertices.removeAll(filteredVertices);
 	}
 
+	/**
+	 * Calculate which vertices can be reached for every vertex
+	 */
 	private void generateNeighbours() {
 		for(Vertex vertex1 : vertices)
 			for(Vertex vertex2 : vertices)
@@ -146,13 +175,25 @@ public class DijkstraPathPlanner {
 					if(!intersectsObject(vertex1, vertex2))
 						vertex1.addNeighbour(vertex2);
 	}
-	
+
+	/**
+	 * Get the square distance between 2 vertices
+	 * @param vertex1
+	 * @param vertex2
+	 * @return square distance
+	 */
 	private int getSquareDistance(Vertex vertex1, Vertex vertex2) {
         int xSquare = (int) Math.pow(vertex1.getPosition().getX() - vertex2.getPosition().getX(), 2);
         int ySquare = (int) Math.pow(vertex1.getPosition().getY() - vertex2.getPosition().getY(), 2);
         return xSquare + ySquare;
 	}
 
+	/**
+	 * calculate if there's an object between two vertices
+	 * @param vertex1
+	 * @param vertex2
+	 * @return true when an object is found between the vertices
+	 */
 	private boolean intersectsObject(Vertex vertex1, Vertex vertex2) {
 		for(Rectangle2D rect : objects)
 			if(rect.intersectsLine(vertex1.getPosition().getX(), vertex1.getPosition().getY(), 
