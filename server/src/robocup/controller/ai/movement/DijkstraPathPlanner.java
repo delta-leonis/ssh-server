d34c9a535bf74b6999443c49564fac70d4d03525
Thijs Emmerink <thijs.emmerink@gmail.com>
2014-11-19 11:23:42 +0100

    Fix the pathplanner, TO-DO: remove vertices if they're out of the playing field

--- server/src/robocup/controller/ai/movement/DijkstraPathPlanner.java
+++ server/src/robocup/controller/ai/movement/DijkstraPathPlanner.java
@@ -20,10 +20,7 @@ public class DijkstraPathPlanner {
 	 * Create the pathplanner
 	 */
 	public DijkstraPathPlanner() {
-		world = World.getInstance();
-		objects = new ArrayList<Rectangle2D>();
-		vertices = new ArrayList<Vertex>();
-		filteredVertices = new ArrayList<Vertex>();
+		reset();
 	}
 	
 	private class Vertex {
@@ -49,6 +46,10 @@ public class DijkstraPathPlanner {
 	    public boolean equals(Object vertex) {
 	        return ((Vertex)vertex).getPosition().getX() == position.getX() && ((Vertex)vertex).getPosition().getY() == position.getY();
 	    }
+	    @Override
+	    public String toString() {
+	    	return position.toString();
+	    }
 	}
 	
 	/**
@@ -60,14 +61,15 @@ public class DijkstraPathPlanner {
 	 */
 	public LinkedList<Point> getRoute(Point beginNode, Point destination, int robotId) {
 		LinkedList<Point> route = new LinkedList<Point>();
-		
+
+		generateObjectList(robotId);
+
 		// no object on route
 		if(!intersectsObject(new Vertex(beginNode), new Vertex(destination))) {
 			route.push(destination);
 			return route;
 		}
 
-		generateObjectList(robotId);
 		generateVertices();
 		removeCollidingVertices();
 		
@@ -81,28 +83,55 @@ public class DijkstraPathPlanner {
 		generateNeighbours();
 		
 		Vertex u = source;
+		calculatePath(source, u, dest);
 		
+		u = dest;
+		while(u.getPrevious() != null) {
+			route.push(u.getPosition());
+			u = u.getPrevious();
+		}
+		
+		this.reset();
+		
+		return route;
+	}
+	
+	/**
+	 * Reset objects
+	 */
+	private void reset() {
+		world = World.getInstance();
+		objects = new ArrayList<Rectangle2D>();
+		vertices = new ArrayList<Vertex>();
+		filteredVertices = new ArrayList<Vertex>();
+	}
+
+	/**
+	 * Calculate the path
+	 * @param source 
+	 * @param u
+	 * @param dest
+	 */
+	private void calculatePath(Vertex source, Vertex u, Vertex dest) {
 		while(vertices.size() > 0) {
-			if(!vertices.contains(source))
+			if(!vertices.contains(source)) {
 				u = getMinDistNeighbour(u);
+			}
+			
+			if(u.equals(dest))
+				return;
 			
 			vertices.remove(u);
 			
 			for(Vertex v : u.getNeighbours()) {
-				int alt = u.getDist() + getSquareDistance(u, v);
+				int alt = u.getDist() + getDistance(u, v);
+				
 				if(alt < v.getDist()) {
 					v.setDist(alt);
 					v.setPrevious(u);
 				}
 			}
 		}
-		
-		u = dest;
-		while(u.getPrevious() != null) {
-			route.push(u.getPosition());
-		}
-		
-		return route;
 	}
 
 	/**
@@ -112,11 +141,11 @@ public class DijkstraPathPlanner {
 	 */
 	private Vertex getMinDistNeighbour(Vertex u) {
 		int minDist = Integer.MAX_VALUE;
-		Vertex closest = null;
+		Vertex closest = u;
 
 		for(Vertex v : u.getNeighbours()) {
-			int dist = getSquareDistance(u, v);
-			if(closest == null || dist < minDist) {
+			int dist = getDistance(u, v);
+			if(closest == null || dist < minDist && !isVertexClosed(v)) {
 				closest = v;
 				minDist = dist;
 			}
@@ -126,16 +155,29 @@ public class DijkstraPathPlanner {
 	}
 
 	/**
+	 * check if a vertex has been used already
+	 * @param v the vertex
+	 * @return true when v is not present in the list of open vertices
+	 */
+	private boolean isVertexClosed(Vertex v) {
+		for(Vertex u : vertices)
+			if(v.equals(u))
+				return false;
+		
+		return true;
+	}
+
+	/**
 	 * Create a rectangle around every robot so we can calculate intersections
 	 * @param robotId the robot id of the robot who needs a path, no rectangle will be created for this robot
 	 */
 	private void generateObjectList(int robotId) {
 		for(Robot r : world.getAlly().getRobots())
 			if(r.getRobotID() != robotId)
-				objects.add(new Rectangle2D.Float(r.getPosition().getX(), r.getPosition().getY(), 399, 399));
+				objects.add(new Rectangle2D.Float(r.getPosition().getX() - 199, r.getPosition().getY() - 199, 398, 398));
 
 		for(Robot r : world.getEnemy().getRobots())
-			objects.add(new Rectangle2D.Float(r.getPosition().getX(), r.getPosition().getY(), 399, 399));
+			objects.add(new Rectangle2D.Float(r.getPosition().getX() - 199, r.getPosition().getY() - 199, 398, 398));
 	}
 
 	/**
@@ -145,7 +187,7 @@ public class DijkstraPathPlanner {
 		for(Rectangle2D rect : objects) {
 			int x = (int) rect.getCenterX();
 			int y = (int) rect.getCenterY();
-
+			
 			vertices.add(new Vertex(new Point(x + DISTANCE_TO_ROBOT, y + DISTANCE_TO_ROBOT)));
 			vertices.add(new Vertex(new Point(x + DISTANCE_TO_ROBOT, y - DISTANCE_TO_ROBOT)));
 			vertices.add(new Vertex(new Point(x - DISTANCE_TO_ROBOT, y + DISTANCE_TO_ROBOT)));
@@ -182,10 +224,10 @@ public class DijkstraPathPlanner {
 	 * @param vertex2
 	 * @return square distance
 	 */
-	private int getSquareDistance(Vertex vertex1, Vertex vertex2) {
-        int xSquare = (int) Math.pow(vertex1.getPosition().getX() - vertex2.getPosition().getX(), 2);
-        int ySquare = (int) Math.pow(vertex1.getPosition().getY() - vertex2.getPosition().getY(), 2);
-        return xSquare + ySquare;
+	private int getDistance(Vertex vertex1, Vertex vertex2) {
+        float dx = vertex1.getPosition().getX() - vertex2.getPosition().getX();
+        float dy = vertex1.getPosition().getY() - vertex2.getPosition().getY();
+        return (int) Math.sqrt(dx*dx + dy*dy);
 	}
 
 	/**
