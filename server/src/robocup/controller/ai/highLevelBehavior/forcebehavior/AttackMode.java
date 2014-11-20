a6405b97d520ff370c96badf4416ec9dca370bc7
CrashO <rob@tikgeit.eu>
2014-11-20 10:29:44 +0100

    gemeenschappelijk modebehavior functies samengevoegd in de base class

--- server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/AttackMode.java
+++ server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/AttackMode.java
@@ -127,7 +127,7 @@ public class AttackMode extends Mode {
 			break;
 
 		case ATTACKER:
-			Point freePosition = getClosestAllyRobotToBall() == robot ? null : getFreePosition(null);
+			Point freePosition = getClosestAllyRobotToBall(world) == robot ? null : getFreePosition(null);
 
 			if (isUpdate) {
 				((Attacker) executer.getLowLevelBehavior()).update(freePosition, ball.getPosition(), 0, 0,
@@ -141,7 +141,7 @@ public class AttackMode extends Mode {
 
 		case BLOCKER: // fuckrobot
 			// Determine closest robot who does not yet have a blocker
-			Robot opponent = getClosestEnemyToRobot(robot, true);
+			Robot opponent = getClosestEnemyToRobot(robot, true, executers);
 
 			int distanceToOpponent = 250;
 
@@ -157,142 +157,5 @@ public class AttackMode extends Mode {
 		}
 	}
 
-	/**
-	 * Find a free position for the robot A position is free when the robot can
-	 * get the ball passed
-	 * 
-	 * @param robot
-	 *            the robot who needs a free position
-	 * @return a free position
-	 */
-	private Point getFreePosition(Robot robot) {
-		return new Point(-500, 0);
-	}
-
-	/**
-	 * Get the closest robot to the ball on our team
-	 * 
-	 * @return closest robot
-	 */
-	private Robot getClosestAllyRobotToBall() {
-		Ball ball = world.getBall();
-		ArrayList<Robot> robots = world.getAlly().getRobots();
-
-		int minDistance = -1;
-		Robot closestRobot = null;
-
-		for (Robot r : robots) {
-			if (minDistance == -1) {
-				closestRobot = r;
-				minDistance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
-			} else {
-				int distance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
-
-				if (distance < minDistance) {
-					closestRobot = r;
-					minDistance = distance;
-				}
-			}
-		}
-
-		return closestRobot;
-	}
-
-	/**
-	 * Calculate if ally team is closer to the ball
-	 * 
-	 * @return true when the ally team is closer
-	 */
-	private boolean allyHasBall() {
-		ArrayList<Robot> allies = world.getAlly().getRobots();
-		ArrayList<Robot> enemies = world.getEnemy().getRobots();
-
-		int distanceAlly = getTeamDistanceToBall(allies);
-		int distanceEnemy = getTeamDistanceToBall(enemies);
-
-		return distanceAlly < distanceEnemy;
-	}
-
-	/**
-	 * Get the distance from the closest robot in one team to the ball
-	 * 
-	 * @param robots
-	 *            the team of robots
-	 * @return the distance of the closest robot
-	 */
-	private int getTeamDistanceToBall(ArrayList<Robot> robots) {
-		int minDistance = -1;
-		Ball ball = world.getBall();
-
-		for (Robot r : robots) {
-			if (minDistance == -1)
-				minDistance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
-			else {
-				int distance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
-
-				if (distance < minDistance) {
-					minDistance = distance;
-				}
-			}
-		}
-
-		return minDistance;
-	}
-
-	/**
-	 * Get the closest enemy robot to robot
-	 * 
-	 * @return closest robot
-	 */
-	private Robot getClosestEnemyToRobot(Robot robot, boolean withoutBlocker) {
-		World world = World.getInstance();
-		ArrayList<Robot> robots = world.getEnemy().getRobots();
-
-		int minDistance = -1;
-		Robot closestRobot = null;
-
-		for (Robot r : robots) {
-			if (minDistance == -1) {
-				closestRobot = r;
-				minDistance = (int) r.getPosition().getDeltaDistance(robot.getPosition());
-			} else {
-				int distance = (int) r.getPosition().getDeltaDistance(robot.getPosition());
-
-				if (distance < minDistance) {
-					if (withoutBlocker) {
-						// Only continue of the robot has no blocker assigned
-						if (!robotHasBlocker(r)) {
-							closestRobot = r;
-							minDistance = distance;
-						}
-					} else {
-						closestRobot = r;
-						minDistance = distance;
-					}
-				}
-			}
-		}
-
-		return closestRobot;
-	}
-
-	/**
-	 * Find out if the robot has a blocker assigned to it
-	 * 
-	 * @param robot
-	 * @return bool if true, a blocker is assigned
-	 */
-	private boolean robotHasBlocker(Robot robot) {
-
-		for (RobotExecuter executer : executers) {
-			if (executer.getLowLevelBehavior().getRole() == roles.BLOCKER) {
-
-				if (robot.getRobotID() == ((FuckRobot) executer.getLowLevelBehavior()).getOpponentId()) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
+	
 }
--- server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/Mode.java
+++ server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/Mode.java
@@ -5,7 +5,12 @@ package robocup.controller.ai.highLevelBehavior.forcebehavior;
 
 import java.util.ArrayList;
 
+import robocup.controller.ai.lowLevelBehavior.FuckRobot;
 import robocup.controller.ai.lowLevelBehavior.RobotExecuter;
+import robocup.model.Ball;
+import robocup.model.Point;
+import robocup.model.Robot;
+import robocup.model.World;
 
 public abstract class Mode {
 
@@ -39,4 +44,146 @@ public abstract class Mode {
 	
 	
 	public abstract void updateExecuters(ArrayList<RobotExecuter> executers) ;
+	
+	
+	
+	
+	/**
+	 * Find a free position for the robot A position is free when the robot can
+	 * get the ball passed
+	 * 
+	 * @param robot
+	 *            the robot who needs a free position
+	 * @return a free position
+	 */
+	protected Point getFreePosition(Robot robot) {
+		return new Point(-500, 0);
+	}
+
+	/**
+	 * Get the closest robot to the ball on our team
+	 * 
+	 * @return closest robot
+	 */
+	protected Robot getClosestAllyRobotToBall(World world) {
+		Ball ball = world.getBall();
+		ArrayList<Robot> robots = world.getAlly().getRobots();
+
+		int minDistance = -1;
+		Robot closestRobot = null;
+
+		for (Robot r : robots) {
+			if (minDistance == -1) {
+				closestRobot = r;
+				minDistance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
+			} else {
+				int distance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
+
+				if (distance < minDistance) {
+					closestRobot = r;
+					minDistance = distance;
+				}
+			}
+		}
+
+		return closestRobot;
+	}
+
+	/**
+	 * Calculate if ally team is closer to the ball
+	 * 
+	 * @return true when the ally team is closer
+	 */
+	protected boolean allyHasBall(World world) {
+		ArrayList<Robot> allies = world.getAlly().getRobots();
+		ArrayList<Robot> enemies = world.getEnemy().getRobots();
+
+		int distanceAlly = getTeamDistanceToBall(world, allies);
+		int distanceEnemy = getTeamDistanceToBall(world, enemies);
+
+		return distanceAlly < distanceEnemy;
+	}
+
+	/**
+	 * Get the distance from the closest robot in one team to the ball
+	 * 
+	 * @param robots
+	 *            the team of robots
+	 * @return the distance of the closest robot
+	 */
+	protected int getTeamDistanceToBall(World world, ArrayList<Robot> robots) {
+		int minDistance = -1;
+		Ball ball = world.getBall();
+
+		for (Robot r : robots) {
+			if (minDistance == -1)
+				minDistance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
+			else {
+				int distance = (int) r.getPosition().getDeltaDistance(ball.getPosition());
+
+				if (distance < minDistance) {
+					minDistance = distance;
+				}
+			}
+		}
+
+		return minDistance;
+	}
+
+	/**
+	 * Get the closest enemy robot to robot
+	 * 
+	 * @return closest robot
+	 */
+	protected Robot getClosestEnemyToRobot(Robot robot, boolean withoutBlocker, ArrayList<RobotExecuter> executers) {
+		World world = World.getInstance();
+		ArrayList<Robot> robots = world.getEnemy().getRobots();
+
+		int minDistance = -1;
+		Robot closestRobot = null;
+
+		for (Robot r : robots) {
+			if (minDistance == -1) {
+				closestRobot = r;
+				minDistance = (int) r.getPosition().getDeltaDistance(robot.getPosition());
+			} else {
+				int distance = (int) r.getPosition().getDeltaDistance(robot.getPosition());
+
+				if (distance < minDistance) {
+					if (withoutBlocker) {
+						// Only continue of the robot has no blocker assigned
+						if (!robotHasBlocker(r, executers)) {
+							closestRobot = r;
+							minDistance = distance;
+						}
+					} else {
+						closestRobot = r;
+						minDistance = distance;
+					}
+				}
+			}
+		}
+
+		return closestRobot;
+	}
+
+	/**
+	 * Find out if the robot has a blocker assigned to it
+	 * 
+	 * @param robot
+	 * @return bool if true, a blocker is assigned
+	 */
+	protected boolean robotHasBlocker(Robot robot, ArrayList<RobotExecuter> executers) {
+
+		for (RobotExecuter executer : executers) {
+			if (executer.getLowLevelBehavior().getRole() == roles.BLOCKER) {
+
+				if (robot.getRobotID() == ((FuckRobot) executer.getLowLevelBehavior()).getOpponentId()) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 }
