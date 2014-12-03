e6260c1af08b70d156eb7177895229dad8b1ef5c
Thijs Emmerink <thijs.emmerink@gmail.com>
2014-12-03 11:25:40 +0100

    correct logic error

--- server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/forces/GoalForce.java
+++ server/src/robocup/controller/ai/highLevelBehavior/forcebehavior/forces/GoalForce.java
@@ -14,6 +14,6 @@ public class GoalForce extends Force {
 		int dist2 = (int) position.getDeltaDistance(world.getField().getGoal().get(0).getFrontRight());
 		int dist3 = (int) position.getDeltaDistance(world.getField().getGoal().get(1).getFrontLeft());
 		int dist4 = (int) position.getDeltaDistance(world.getField().getGoal().get(1).getFrontRight());
-		return dist1 < scope && dist2 < scope && dist3 < scope && dist4 < scope;
+		return dist1 < scope && dist2 < scope || dist3 < scope && dist4 < scope;
 	}
 }
