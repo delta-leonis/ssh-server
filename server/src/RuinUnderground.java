858eeacb4b665ffba1986e42f67ecdaf2e7fd752
Server <you@example.com>
2015-03-06 15:23:45 +0100

    added widget to let the selected robot spin (will invert speed every second)

---
+++ server/src/RuinUnderground.java
@@ -0,0 +1,18 @@
+import robocup.output.ComInterface;
+import robocup.output.RobotCom;
+
+
+public class RuinUnderground {
+	public static void main(String[] args) throws InterruptedException {
+		boolean richting = false;
+		try{
+			while(true){
+				System.out.println("LOOP");
+			//
+				Thread.sleep(500);
+				richting = !richting;
+			}
+		}finally{
+		}
+	}
+}
