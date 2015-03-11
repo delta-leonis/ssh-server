9e872e7d1d77a69569ff1770641c484c8875c913
Thomas <xaldini@live.nl>
2015-03-11 11:18:43 +0100

    Added keylisteners. #Jeroen #KeyMaster

--- server/src/robocup/view/widgets/ControlRobotWidget.java
+++ server/src/robocup/view/widgets/ControlRobotWidget.java
@@ -2,6 +2,8 @@ package robocup.view.widgets;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.logging.Logger;
 
 import javax.swing.JButton;
@@ -16,13 +18,13 @@ import robocup.view.GUI;
 import robocup.view.WidgetBox;
 
 @SuppressWarnings("serial")
-public class ControlRobotWidget extends WidgetBox {
+public class ControlRobotWidget extends WidgetBox{
 
 	private static Logger LOGGER = Logger.getLogger(Main.class.getName());
 	private JLabel selectedRobotLabel;
 	private int selectedRobotId;
 	private boolean dribbling = false;
-
+	
 	/**
 	 * Create ControLRobotWidget
 	 */
@@ -58,6 +60,10 @@ public class ControlRobotWidget extends WidgetBox {
 		add(moveLeftButton, "growx");
 		add(moveBackButton, "growx");
 		add(moveRightButton, "growx");
+
+		addKeyListener(new MoveKeyListener());
+		setFocusable(true);
+		requestFocusInWindow();
 	}
 
 	/**
@@ -86,12 +92,47 @@ public class ControlRobotWidget extends WidgetBox {
 			
 		}
 	}
+	
+	private class MoveKeyListener implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e){
+			int code = e.getKeyCode();
+			switch(code){
+				case KeyEvent.VK_UP:
+					ComInterface.getInstance(RobotCom.class).send(1, selectedRobotId, 0, 200, 0, 0, 0, 0, dribbling);
+					break;
+				case KeyEvent.VK_DOWN:
+					ComInterface.getInstance(RobotCom.class).send(1, selectedRobotId, 0, -200, 0, 0, 0, 0, dribbling);
+					break;
+				case KeyEvent.VK_LEFT:
+					ComInterface.getInstance(RobotCom.class).send(1, selectedRobotId, 0, 0, 0, 0, 20, 0, dribbling);
+					break;
+				case KeyEvent.VK_RIGHT:
+					ComInterface.getInstance(RobotCom.class).send(1, selectedRobotId, 0, 0, 0, 0, -20, 0, dribbling);
+					break;
+				case KeyEvent.VK_ESCAPE:
+					for(int i = 0; i < 12; ++i){
+						ComInterface.getInstance(RobotCom.class).send(1, i, 0, 0, 0, 0, 0,0, false);
+					}
+					break;
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {			
+		}
+	}
 
 	/**
 	 * Update the selected robot panel
 	 */
 	@Override
 	public void update() {
+		requestFocusInWindow();
 		selectedRobotId = ((GUI) SwingUtilities.getWindowAncestor((this))).getSelectedRobotId();
 		selectedRobotLabel.setText(String.format("Robot #%d selected", selectedRobotId));
 	}
