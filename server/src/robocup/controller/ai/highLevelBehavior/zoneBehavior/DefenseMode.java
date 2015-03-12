60e68941f1f26ca53c3c682dd6872fbacbb4e000
Thijs Emmerink <thijs.emmerink@gmail.com>
2015-03-12 12:23:46 +0100

    Implement basic structure for ZoneBehavior classes, needs new functions for new lowlevelbehavior classes, needs implementation for update functions and determineRole

--- server/src/robocup/controller/ai/highLevelBehavior/zoneBehavior/DefenseMode.java
+++ server/src/robocup/controller/ai/highLevelBehavior/zoneBehavior/DefenseMode.java
@@ -2,35 +2,50 @@ package robocup.controller.ai.highLevelBehavior.zoneBehavior;
 
 import java.util.ArrayList;
 
+import robocup.controller.ai.lowLevelBehavior.Attacker;
+import robocup.controller.ai.lowLevelBehavior.Keeper;
+import robocup.controller.ai.lowLevelBehavior.KeeperDefender;
 import robocup.controller.ai.lowLevelBehavior.RobotExecuter;
+import robocup.controller.ai.lowLevelBehavior.Stoorder;
+import robocup.model.enums.RobotMode;
 
 public class DefenseMode extends Mode {
 
 	public DefenseMode(ArrayList<RobotExecuter> executers) {
-		// TODO Auto-generated constructor stub
+		super(executers);
 	}
 
 	@Override
-	public void handleAttacker(RobotExecuter executer) {
-		// TODO Auto-generated method stub
-
+	public RobotMode determineRole(RobotExecuter executer) {
+		// TODO Determine role based on positions and zones on the field
+		return RobotMode.DEFENDER;
 	}
 
 	@Override
-	public void handleBlocker(RobotExecuter executer) {
-		// TODO Auto-generated method stub
-
+	public void updateAttacker(RobotExecuter executer) {
+		Attacker attacker = (Attacker) executer.getLowLevelBehavior();
+		// TODO Update with normal values
+		attacker.update(null, null, 0, false, 0);
 	}
 
 	@Override
-	public void handleDefender(RobotExecuter executer) {
-		// TODO Auto-generated method stub
-
+	public void updateBlocker(RobotExecuter executer) {
+		Stoorder blocker = (Stoorder) executer.getLowLevelBehavior();
+		// TODO Update with normal values
+		blocker.update(250, null, null, null, 0);
 	}
 
 	@Override
-	public void handleKeeper(RobotExecuter executer) {
-		// TODO Auto-generated method stub
+	public void updateKeeperDefender(RobotExecuter executer) {
+		KeeperDefender keeperDefender = (KeeperDefender) executer.getLowLevelBehavior();
+		// TODO Update with normal values
+		keeperDefender.update(0, false, null, null);
+	}
 
+	@Override
+	public void updateKeeper(RobotExecuter executer) {
+		Keeper keeper = (Keeper) executer.getLowLevelBehavior();
+		// TODO Update with normal values
+		keeper.update(0, false, null, null);
 	}
 }
--- server/src/robocup/controller/ai/highLevelBehavior/zoneBehavior/Mode.java
+++ server/src/robocup/controller/ai/highLevelBehavior/zoneBehavior/Mode.java
@@ -2,15 +2,37 @@ package robocup.controller.ai.highLevelBehavior.zoneBehavior;
 
 import java.util.ArrayList;
 
+import robocup.controller.ai.lowLevelBehavior.Attacker;
+import robocup.controller.ai.lowLevelBehavior.Keeper;
+import robocup.controller.ai.lowLevelBehavior.KeeperDefender;
 import robocup.controller.ai.lowLevelBehavior.RobotExecuter;
+import robocup.controller.ai.lowLevelBehavior.Stoorder;
+import robocup.model.Ally;
+import robocup.model.Ball;
+import robocup.model.World;
 import robocup.model.enums.RobotMode;
+import robocup.output.ComInterface;
+import robocup.output.RobotCom;
 
 public abstract class Mode {
 
+	protected World world;
+	protected Ball ball;
+
+	public Mode(ArrayList<RobotExecuter> executers) {
+		world = World.getInstance();
+		ball = world.getBall();
+	}
+
+	/**
+	 * Execute the mode.
+	 * New roles will be assigned to every robot and their lowlevel behaviors will be updated.
+	 * @param executers
+	 */
 	public void execute(ArrayList<RobotExecuter> executers) {
 		try {
 			for (RobotExecuter executer : executers) {
-				updateExecuter(executer, determineRole());
+				updateExecuter(executer, determineRole(executer));
 			}
 		} catch (Exception e) {
 			System.out.println("Exception in Mode, please fix me :(");
@@ -18,12 +40,26 @@ public abstract class Mode {
 		}
 	}
 
-	private RobotMode determineRole() {
-		// TODO determine role for a robot
-		return RobotMode.ATTACKER;
-	}
+	/**
+	 * Determine the role for an executer.
+	 * Role will be chosen based on current strategy and mode.
+	 * @return role of the robot
+	 */
+	public abstract RobotMode determineRole(RobotExecuter executer);
 
+	/**
+	 * Update an executer.
+	 * A role will be assigned to the robot belonging to this executer.
+	 * A new lowlevel behavior will be created if the role is different from the previous role.
+	 * The lowlevel behavior will be updated as well.
+	 * @param executer the executer to update
+	 * @param role the role which needs to be assigned to the robot belonging to the executer
+	 */
 	public void updateExecuter(RobotExecuter executer, RobotMode role) {
+		// Set Role for Robot
+		((Ally) executer.getRobot()).setRole(role);
+
+		// Execute handle functions based on role
 		switch (role) {
 		case ATTACKER:
 			handleAttacker(executer);
@@ -32,7 +68,7 @@ public abstract class Mode {
 			handleBlocker(executer);
 			break;
 		case DEFENDER:
-			handleDefender(executer);
+			handleKeeperDefender(executer);
 			break;
 		case KEEPER:
 			handleKeeper(executer);
@@ -42,11 +78,83 @@ public abstract class Mode {
 		}
 	}
 
-	public abstract void handleAttacker(RobotExecuter executer);
+	/**
+	 * Handle the behavior of the Attacker.
+	 * A new Attacker behavior will be created if the current lowlevel behavior is not an Attacker.
+	 * Update the values of the Attacker afterwards.
+	 * @param executer the executer which needs to be handled
+	 */
+	public void handleAttacker(RobotExecuter executer) {
+		if (!(executer.getLowLevelBehavior() instanceof Attacker))
+			executer.setLowLevelBehavior(new Attacker(executer.getRobot(), ComInterface.getInstance(RobotCom.class),
+					null, null, 0, false, 0));
 
-	public abstract void handleBlocker(RobotExecuter executer);
+		updateAttacker(executer);
+	}
 
-	public abstract void handleDefender(RobotExecuter executer);
+	/**
+	 * Update the values of the Attacker behavior belonging to the executer.
+	 * @param executer the executer to update
+	 */
+	public abstract void updateAttacker(RobotExecuter executer);
 
-	public abstract void handleKeeper(RobotExecuter executer);
+	/**
+	 * Handle the behavior of the Blocker.
+	 * A new Blocker behavior will be created if the current lowlevel behavior is not a Blocker.
+	 * Update the values of the Blocker afterwards.
+	 * @param executer the executer which needs to be handled
+	 */
+	public void handleBlocker(RobotExecuter executer) {
+		if (!(executer.getLowLevelBehavior() instanceof Stoorder))
+			executer.setLowLevelBehavior(new Stoorder(executer.getRobot(), ComInterface.getInstance(RobotCom.class),
+					250, null, null, null, 0));
+
+		updateBlocker(executer);
+	}
+
+	/**
+	 * Update the values of the Blocker behavior belonging to the executer.
+	 * @param executer the executer to update
+	 */
+	public abstract void updateBlocker(RobotExecuter executer);
+
+	/**
+	 * Handle the behavior of the KeeperDefender.
+	 * A new Attacker behavior will be created if the current lowlevel behavior is not a KeeperDefender.
+	 * Update the values of the KeeperDefender afterwards.
+	 * @param executer the executer which needs to be handled
+	 */
+	public void handleKeeperDefender(RobotExecuter executer) {
+		if (!(executer.getLowLevelBehavior() instanceof KeeperDefender))
+			executer.setLowLevelBehavior(new KeeperDefender(executer.getRobot(), ComInterface
+					.getInstance(RobotCom.class), 1200, false, null, null, null, null, 0));
+
+		updateKeeperDefender(executer);
+	}
+
+	/**
+	 * Update the values of the KeeperDefender behavior belonging to the executer.
+	 * @param executer the executer to update
+	 */
+	public abstract void updateKeeperDefender(RobotExecuter executer);
+
+	/**
+	 * Handle the behavior of the Keeper.
+	 * A new Keeper behavior will be created if the current lowlevel behavior is not a Keeper.
+	 * Update the values of the Keeper afterwards.
+	 * @param executer the executer which needs to be handled
+	 */
+	public void handleKeeper(RobotExecuter executer) {
+		if (!(executer.getLowLevelBehavior() instanceof Keeper))
+			executer.setLowLevelBehavior(new Keeper(executer.getRobot(), ComInterface.getInstance(RobotCom.class), 500,
+					false, null, null, null, 0));
+
+		updateKeeper(executer);
+	}
+
+	/**
+	 * Update the values of the Keeper behavior belonging to the executer.
+	 * @param executer the executer to update
+	 */
+	public abstract void updateKeeper(RobotExecuter executer);
 }
