6420d7acb70f7734a7f338ff0be2ba9126db0d79
Thijs Emmerink <thijs.emmerink@gmail.com>
2015-04-02 12:07:51 +0200

    Format enums

--- server/src/robocup/model/enums/TeamColor.java
+++ server/src/robocup/model/enums/TeamColor.java
@@ -2,9 +2,9 @@ package robocup.model.enums;
 
 public enum TeamColor {
 	YELLOW, BLUE;
-	
-	public java.awt.Color toColor(){
-		if(this.equals(TeamColor.BLUE)){
+
+	public java.awt.Color toColor() {
+		if (this.equals(TeamColor.BLUE)) {
 			return java.awt.Color.blue;
 		}
 		return java.awt.Color.yellow;
