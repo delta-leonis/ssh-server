b0f5e800ca90c44a73fbaf842234c8a68ba44302
Thijs Emmerink <thijs.emmerink@gmail.com>
2015-05-19 14:14:25 +0200

    Fix ghosting, please clean me

--- server/src/robocup/model/FieldObject.java
+++ server/src/robocup/model/FieldObject.java
@@ -6,8 +6,6 @@ package robocup.model;
  */
 public abstract class FieldObject {
 
-	private FieldPoint positionCam0;
-	private FieldPoint positionCam1;
 	private FieldPoint position;
 	// LastUpdateTime = time off the day in sec
 	protected double lastUpdateTime;
@@ -17,8 +15,6 @@ public abstract class FieldObject {
 	public FieldObject() {
 		lastUpdateTime = 0;
 		position = null;
-		positionCam0 = null;
-		positionCam1 = null;
 	}
 
 	/**
@@ -28,77 +24,32 @@ public abstract class FieldObject {
 	 * @post Updated position, direction and speed.
 	 */
 	public void update(FieldPoint newPosition, double updateTime, int camUpdateNo) {
-		double newTime = updateTime;
-
-		// System.out.println(newPosition);
-		FieldPoint tmpPosition;
-		if (!correctCamSide(newPosition.getX(), newPosition.getY(), camUpdateNo)) {
-			if (camUpdateNo == 0 && positionCam1 == null) {
-				;
-			} else if (camUpdateNo == 1 && positionCam0 == null) {
-				;
-			} else if (positionCam0 == null && positionCam1 == null) {
-				;
-			} else {
-				;
-			}
-		}
-		if (camUpdateNo == 0) {
-			positionCam0 = new FieldPoint(newPosition.getX(), newPosition.getY());
-			// System.out.println(camUpdateNo + "# " + positionCam0.getX());
-		}
-		if (camUpdateNo == 1) {
-			positionCam1 = new FieldPoint(newPosition.getX(), newPosition.getY());
-			// System.out.println(camUpdateNo + "# " + positionCam1.getX());
+		switch (camUpdateNo) {
+		case 0:
+			if (newPosition.getX() >= 0 || newPosition.getY() <= 0)
+				return;
+			break;
+		case 1:
+			if (newPosition.getX() <= 0 || newPosition.getY() >= 0)
+				return;
+			break;
+		case 2:
+			if (newPosition.getX() <= 0 || newPosition.getY() <= 0)
+				return;
+			break;
+		case 3:
+			if (newPosition.getX() >= 0 || newPosition.getY() >= 0)
+				return;
+			break;
 		}
 
-		if (World.getInstance().getField().getCameraOverlapZoneWidth() > Math.abs(newPosition.getX())) {
-			if (this instanceof Ball) {
-				if ((positionCam0 != null && positionCam1 != null)) {
-
-					double newX = positionCam0.getX();
-					double newY = positionCam0.getY();
-					if (Math.abs(positionCam1.getX()) > Math.abs(newX)
-							&& Math.abs(positionCam1.getY()) > Math.abs(newY)) {
-						newX = positionCam1.getX();
-						newY = positionCam1.getY();
-						positionCam1 = null;
-					} else {
-						positionCam0 = null;
-					}
-
-					// System.out.println("isbal: " + (this instanceof Ball));
-					tmpPosition = new FieldPoint(newX, newY);
-				} else {
-					tmpPosition = newPosition;
-				}
-			} else {
-				tmpPosition = newPosition;
-			}
-		} else {
-			tmpPosition = newPosition;
-		}
-
-		// if(this instanceof Robot) {
-		// System.out.println(" Robo loc: " + tmpPosition);
-		// }
-
-		// lastCamUpdateNo = camUpdateNo;
 		if (position != null) {
-			setDirection(tmpPosition);
-			setSpeed(newTime, tmpPosition);
+			setDirection(newPosition);
+			setSpeed(updateTime, newPosition);
 		}
 
-		position = tmpPosition;
-		lastUpdateTime = newTime;
-	}
-
-	public boolean correctCamSide(double d, double e, int camNo) {
-		if (d > 0 && camNo == 1)
-			return true;
-		if (d < 0 && camNo == 0)
-			return true;
-		return false;
+		position = newPosition;
+		lastUpdateTime = updateTime;
 	}
 
 	/**
