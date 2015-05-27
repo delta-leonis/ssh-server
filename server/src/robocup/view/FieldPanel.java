d97239e0fc2756197b838b4f5883bddddea451d7
Jeroen de Jong <beheer@jeroendj.nl>
2015-05-27 16:00:18 +0200

    Fixed framerate issue that will fix the jittering in the fielddrawing. also added an FPS counter, because, PCMR!

--- server/src/robocup/view/FieldPanel.java
+++ server/src/robocup/view/FieldPanel.java
@@ -55,6 +55,12 @@ public class FieldPanel extends JPanel {
 	private boolean drawNeighbours;
 	private boolean drawVertices;
 
+	private int frameCount;
+
+	private int FPS;
+
+	private long lastFPSpaint;
+
 	/**
 	 * Constructor of {@link FieldPanel}. The panel size is set and the mouseListener is added for 
 	 * the ball position.
@@ -117,18 +123,26 @@ public class FieldPanel extends JPanel {
 		drawRobots(g, ratio);
 		drawBall(g, ratio);
 		drawCoords(g, ratio);
+		drawFPS(g, ratio);
+	}
+	
+	private void drawFPS(Graphics g, double ratio){
+		g.setFont(new Font(g.getFont().getFontName(), Font.BOLD, (int) 22));
+		g.setColor(Color.YELLOW);
+		g.drawString("FPS: " + FPS, getWidth() - 100, 25);
 	}
 
 	/**
-	 * Function for updating the {@link FieldPanel}. Updates with a result of 2 times
-	 * per second at a frame rate of 60 fps
+	 * Function for updating the {@link FieldPanel}.
 	 */
 	public void update() {
-		updateCounter ++;
-		if(updateCounter < 10)
-			return;
+		frameCount++;
+		if(lastFPSpaint + 1000 < System.currentTimeMillis()){
+			lastFPSpaint = System.currentTimeMillis();
+			FPS = frameCount;
+			frameCount = 0;
+		}
 		repaint();
-		updateCounter = 0;
 	}
 
 	/**
@@ -290,8 +304,9 @@ public class FieldPanel extends JPanel {
 	 */
 	private void drawCoord(Graphics g, FieldPoint point, double ratio, int yoffset){
 		Point2D.Double guiPoint = point.toGUIPoint(ratio, mirror);
+		g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, (int) 12));
 		String coordinate = String.format("[%.0f, %.0f]", point.getX(), point.getY());
-		g.drawString(coordinate, (int)guiPoint.getX() - (coordinate.length()/2)*7  + spaceBufferX, (int)guiPoint.getY() + yoffset + (yoffset > 0 ? 5 : 0) + spaceBufferY);
+		g.drawString(coordinate, (int)guiPoint.getX() - (coordinate.length()/2)*(g.getFont().getSize()/2)  + spaceBufferX, (int)guiPoint.getY() + yoffset + (yoffset > 0 ? 5 : 0) + spaceBufferY);
 	}
 	
 	/**
