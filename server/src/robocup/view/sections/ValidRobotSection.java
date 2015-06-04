c79d22117a2e7db97b6a241e1aad04fc7d6f99ab
Jeroen de Jong <beheer@jeroendj.nl>
2015-06-04 12:19:52 +0200

    Made an autoselect for valid robot id's (takes a sample of 0.5 seconds and will select every robot that has been spotted in that sample

--- server/src/robocup/view/sections/ValidRobotSection.java
+++ server/src/robocup/view/sections/ValidRobotSection.java
@@ -3,7 +3,11 @@ package robocup.view.sections;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 
@@ -22,6 +26,7 @@ import robocup.view.SectionBox;
 public class ValidRobotSection extends SectionBox {
 
 	private ArrayList<JCheckBox> checkboxes = new ArrayList<JCheckBox>();
+	private Timer autoselectTimer;
 
 	/**
 	 * Creates a {@link JCheckBox} for every {@link Ally} and 
@@ -33,7 +38,27 @@ public class ValidRobotSection extends SectionBox {
 
 		int numberOfRobots = World.getInstance().getReferee().getAlly().getRobots().size();
 
-		add(new JLabel("Ally IDs"), "span 6, growx, wrap");
+		add(new JLabel("Ally IDs"), "span 4, growx");
+		
+
+		add(new JButton(new AbstractAction("Autoselect") {
+			/**
+			 * Loops all existing {@link Ally}s, when {@link Robot} is not onsight it is 
+			 * removed from view
+			 */
+			public void actionPerformed(ActionEvent e) {
+				for(JCheckBox box : checkboxes){
+					int id = Integer.valueOf(box.getText().substring(1));
+					(box.getName().equals("ally") ? World.getInstance().getValidAllyIDs() : World.getInstance().getValidEnemyIDs()).add(id);
+					box.setEnabled(false);
+				}
+
+				autoselectTimer = new Timer();
+				autoselectTimer.schedule(new SelectTimer(), 500);
+				
+			}
+		}), "span 2, growx, wrap");
+
 		for (int i = 0; i < numberOfRobots; i++) {
 			JCheckBox checkbox = new JCheckBox("#" + i);
 			checkbox.setName("ally");
@@ -42,7 +67,6 @@ public class ValidRobotSection extends SectionBox {
 			checkboxes.add(checkbox);
 			add(checkbox, (numberOfRobots - 1 == i) ? "wrap" : "");
 		}
-		
 		add(new JLabel("Enemy IDs"), "span 6, growx, wrap");
 		
 		numberOfRobots = World.getInstance().getReferee().getEnemy().getRobots().size();
@@ -53,6 +77,27 @@ public class ValidRobotSection extends SectionBox {
 			checkbox.addActionListener(new checkHandler());
 			checkboxes.add(checkbox);
 			add(checkbox, (numberOfRobots - 1 == i) ? "wrap" : "");
+		}
+	}
+
+	class SelectTimer extends TimerTask {
+		public void run() {
+			for(JCheckBox box : checkboxes){
+				//id uitzoeken
+				int id = Integer.valueOf(box.getText().substring(1));
+				//team uitzoeken
+				Team team = (box.getName().equals("ally") ? World.getInstance().getReferee().getAlly() : World.getInstance().getReferee().getEnemy());
+				//onsight setten op checkbox
+				box.setSelected(team.getRobotByID(id).isOnSight());
+				if(!box.isSelected()){
+					//eventueel uit de valid reeks weghalen
+					(box.getName().equals("ally") ? World.getInstance().getValidAllyIDs() : World.getInstance().getValidEnemyIDs()).remove((Integer)id);
+					team.getRobotByID(id).setPosition(null);
+				}
+
+				//enabulun
+				box.setEnabled(true);
+			}
 		}
 	}
 
