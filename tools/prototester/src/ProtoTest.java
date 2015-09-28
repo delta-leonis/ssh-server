cc70a237483d7caa06796175b5af5ed0d8d147bd
Jeroen de Jong <beheer@jeroendj.nl>
2015-09-28 17:35:46 +0200

    Initial commit

---
+++ tools/prototester/src/ProtoTest.java
@@ -0,0 +1,112 @@
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+import output.UDPCommunicator;
+import protobuf.Radio.RadioProtocolCommand;
+import protobuf.Radio.RadioProtocolWrapper;
+
+public class ProtoTest {
+	private static String	DEFAULT_IP				= "192.168.1.10";
+	private static int 		DEFAULT_COMMAND_COUNT 	= 3;  
+	private static int 		DEFAULT_WRAPPER_COUNT 	= 2;  
+	private static int 		DEFAULT_TIMEOUT	 		= 0; 
+	private static int 		DEFAULT_PORT	 		= 2002; 
+	
+	
+	public static void main(String[] args) throws Exception {
+		HashMap<String, String> commands = parseArguments(args);
+		
+		if(commands.containsKey("?") || commands.size() == 0)
+		{
+			System.out.println("Usage:");
+			System.out.printf("-i	defines ip (default: %s)\n", DEFAULT_IP);
+			System.out.printf("-p	defines port (default: %d)\n", DEFAULT_PORT);
+			System.out.printf("-c	defines number of commands per wrapper(default: %d)\n", DEFAULT_COMMAND_COUNT);
+			System.out.printf("-w	defines number of wrappers (default: %d)\n", DEFAULT_WRAPPER_COUNT );
+			System.out.printf("-t	defines timout (default: %d)\n", DEFAULT_TIMEOUT );
+			System.exit(0);
+		}
+		
+		try {
+			//get settings from arguments, otherwise use DEFAULT_* settings
+			InetAddress ip	  = InetAddress.getByName(commands.containsKey("i") ? commands.get("i") : DEFAULT_IP);
+			int command_count = commands.containsKey("c") ? Integer.parseInt(commands.get("c")) : DEFAULT_COMMAND_COUNT;
+			int wrapper_count = commands.containsKey("w") ? Integer.parseInt(commands.get("w")) : DEFAULT_WRAPPER_COUNT;
+			int port		  = commands.containsKey("p") ? Integer.parseInt(commands.get("p")) : DEFAULT_PORT;
+			int timeout		  = commands.containsKey("t") ? Integer.parseInt(commands.get("t")) : DEFAULT_TIMEOUT;
+	
+			//initialize a communicator with received settings
+			UDPCommunicator Communicator = new UDPCommunicator(ip, port);
+			
+			//fill arraylist with wrappers
+			ArrayList<RadioProtocolWrapper.Builder> wrappers = new ArrayList<RadioProtocolWrapper.Builder>();
+			for(int wrapperI = 0; wrapperI < wrapper_count; wrapperI++){
+
+				RadioProtocolWrapper.Builder wrapper = RadioProtocolWrapper.newBuilder();
+				
+				//fill command protobuf with random data
+				for(int commandI = 0; commandI < command_count; commandI++){
+					//add required data
+					RadioProtocolCommand.Builder command = RadioProtocolCommand.newBuilder().setRobotId(commandI)
+							 .setVelocityX((float) Math.random())
+							 .setVelocityY((float) Math.random())
+							 .setVelocityR((float) Math.random());
+					//optional data
+					if(Math.random() > 0.5)
+							 command.setFlatKick((float) (Math.random() * 4.0))
+							 .setChipKick((float) (Math.random() * 4.0));
+					//optional data
+					if(Math.random() > 0.5)
+							 command.setDribblerSpin((float) Math.random());
+					//optional data
+					if(Math.random() > 0.5)
+							 command.setDistance((int) (Math.random() * 40));
+
+					wrapper.addCommand(command);
+				}
+				wrappers.add(wrapper);
+			}
+			
+			wrappers.stream().forEach(e -> Communicator.send(e.build(), timeout));
+
+		} catch (SocketException uhe) {
+			System.err.printf("Socket already in use.\n");
+		} catch (UnknownHostException uhe) {
+			System.err.printf("Unknown host exception.\n");
+		}
+		System.exit(0);
+	}
+
+	/**
+	 * Parse given string[] to HashMap with accumulators and thier arguments,<br>
+	 * accumulators are defined with the starting char '-'.<br>
+	 * parses "value1 value2 -key value3 value4" to<br> 
+	 * value1 => null <br> 
+	 * value2 => null <br> 
+	 * key => value3 <br> 
+	 * value4 => null <br>
+	 * 
+	 * @param args	the string[] with
+	 * @return	parsed commands
+	 */
+	private static HashMap<String, String> parseArguments(String[] args) {
+		//parse command line options
+		HashMap<String, String> opts = new HashMap<String, String>();
+		Stream.of(args).reduce(null, (accumulator, value) ->
+			//Check if the value should be a accumulator (e.a. it has arguments)
+			value.startsWith("-") 
+				? value.substring(1) //return the value to the accumulator var
+				: (accumulator == null  //value doesn't have arguments
+					? opts.put(value, null)
+					//put accumulator and value in opts
+					: opts.put(accumulator, value)
+				  )
+		);
+		return opts;
+	}
+
+}
---
+++ tools/prototester/src/output/UDPCommunicator.java
@@ -0,0 +1,35 @@
+package output;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import protobuf.Radio.RadioProtocolWrapper;
+
+public class UDPCommunicator {
+	private InetAddress ipAddress;
+	private int port;
+	private DatagramSocket socket;
+
+	public UDPCommunicator(InetAddress ip, int port) throws SocketException {
+		this.port = port;
+		this.ipAddress = ip;
+		socket = new DatagramSocket();
+	}
+
+	public void send(RadioProtocolWrapper pbPacket, int timeout){
+		System.out.printf("Sending wrapper with %d commands (total of %d bytes) to %s:%d\n", pbPacket.getCommandCount(), pbPacket.getSerializedSize(), ipAddress, port);
+		DatagramPacket UDPpacket = new DatagramPacket(pbPacket.toByteArray(), pbPacket.getSerializedSize(), ipAddress, port);
+		try {
+			socket.send(UDPpacket);
+			Thread.sleep(timeout);
+		} catch (IOException e) {
+			System.err.printf("Could not send packet to %s:%d.\n", ipAddress, port);
+			//e.printStackTrace(System.err);
+		} catch (InterruptedException e) {
+			System.err.println("Interrupt error occured while waiting.");
+		}
+	}
+}
