af93854e2af22ec270029d1fed94b5839b89de7f
Thoma <ThomasHakkers@hotmail.com>
2015-11-04 15:17:24 +0100

    Updated old Timer classes to have a deprecated tag

--- src/ui/components/timerslider/TimerPane.java
+++ src/ui/components/timerslider/TimerPane.java
@@ -2,6 +2,7 @@ package ui.components.timerslider;
 
 import java.io.IOException;
 
+import examples.TimerSliderExample;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -12,37 +13,40 @@ import javafx.scene.layout.VBox;
 import ui.components.BorderSlideBar;
 
 /**
- * Example pane that partially implements the bottom bar of the end-implementation.
+ * Example pane that partially implements the bottom bar of the
+ * end-implementation.
  * 
+ * @Deprecated Please remove as soon as possible. Don't forget to update
+ *             {@link TimerSliderExample} first
  * @author Thomas Hakkers E-mail: ThomasHakkers@hotmail.com
  *
  */
-public class TimerPane extends VBox{
-	private Button slideBarButton;
-	private Parent slider;
-	private BorderSlideBar borderSlideBar;
-	private HBox root;
-	
-	public TimerPane(){
-		root = new HBox();
-		root.setStyle("-fx-background-color: \"white\";");
-		root.setSpacing(10);
-		slideBarButton = new Button("^");
-		slideBarButton.setStyle("-fx-font: 16px \"Serif\";");
-		slideBarButton.prefHeightProperty().bind(root.heightProperty());
-		
-		try {
-			slider = FXMLLoader.load(getClass().getResource("/fxml/TimeSlider.fxml"));
-			slider.getStylesheets().add(getClass().getResource("/css/slider.css").toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		Label blueLabel = new Label("MIAUW.");
+public class TimerPane extends VBox {
+    private Button slideBarButton;
+    private Parent slider;
+    private BorderSlideBar borderSlideBar;
+    private HBox root;
+
+    public TimerPane() {
+        root = new HBox();
+        root.setStyle("-fx-background-color: \"white\";");
+        root.setSpacing(10);
+        slideBarButton = new Button("^");
+        slideBarButton.setStyle("-fx-font: 16px \"Serif\";");
+        slideBarButton.prefHeightProperty().bind(root.heightProperty());
+
+        try {
+            slider = FXMLLoader.load(getClass().getResource("/fxml/TimeSlider.fxml"));
+            slider.getStylesheets().add(getClass().getResource("/css/slider.css").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Label blueLabel = new Label("MIAUW.");
         blueLabel.setStyle("-fx-background-color: rgba(0, 100, 100, 0.5); -fx-background-radius: 10;");
-		borderSlideBar = new BorderSlideBar(100, slideBarButton, Pos.BOTTOM_CENTER, blueLabel);
-		
-		root.getChildren().addAll(slideBarButton, slider);
-		getChildren().addAll(borderSlideBar, root);
-	}
+        borderSlideBar = new BorderSlideBar(100, slideBarButton, Pos.BOTTOM_CENTER, blueLabel);
+
+        root.getChildren().addAll(slideBarButton, slider);
+        getChildren().addAll(borderSlideBar, root);
+    }
 }
