1f3dae50954586c986cae5e685f16c0eb0174709
Thoma <ThomasHakkers@hotmail.com>
2015-11-05 11:46:44 +0100

    Removed whitespace

--- src/ui/lua/console/ConsoleOutput.java
+++ src/ui/lua/console/ConsoleOutput.java
@@ -6,17 +6,17 @@ import java.io.OutputStream;
 /**
  * Custom OutputStream class, so that we don't bloat the entire Sytem.out
  * Use this stream to write to the {@link Console} instead of the System.out
- * 
+ *
  * @author Thomas Hakkers E-mail: ThomasHakkers@hotmail.com
- * 
+ *
  */
 public class ConsoleOutput extends OutputStream{
 	private Console console;
-	
+
 	public ConsoleOutput(Console console){
 		this.console = console;
 	}
-	
+
 	@Override
 	public void write(int b) throws IOException {
 		console.print(String.valueOf((char)b));
