c10dc7c0b11fba56cb356770a94e302b4eba55bf
marklef2 <marklefering@gmail.com>
2015-11-08 15:17:05 +0100

    [0002 review-m] PenaltySpotGO added comment, added javadoc, add runlater, renamed methods

--- src/org/ssh/field3d/gameobjects/PenaltySpotGO.java
+++ src/org/ssh/field3d/gameobjects/PenaltySpotGO.java
@@ -4,85 +4,141 @@ import org.ssh.field3d.core.game.Game;
 import org.ssh.field3d.core.gameobjects.GameObject;
 import org.ssh.field3d.core.math.Vector3f;
 
+import javafx.application.Platform;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.transform.Rotate;
 
-// TODO: Javadoc, cleanup, comment
+
+/**
+ * PenaltySpotGO class. This is the class for a penalty spot game object. It represents a penalty
+ * spot on the 3d field.
+ * 
+ * @see GameObject
+ *      
+ * @author Mark Lefering
+ */
 public class PenaltySpotGO extends GameObject {
     
-    private Vector3f     _location;
-    private final Circle _model;
+    private Vector3f     location;
+    private final Circle model;
                          
-    private double       _radius;
+    private double       radius;
                          
+    /**
+     * Constructor
+     * 
+     * @param game
+     *            The {@link Game} of the {@link GameObject}.
+     * @param location
+     *            The location of the penalty spot as {@link Vector3f}.
+     * @param radius
+     *            The radius of the penalty spot.
+     */
     public PenaltySpotGO(final Game game, final Vector3f location, final double radius) {
         
+        // Initialize super class
         super(game);
         
         // Creating new circle
-        this._model = new Circle(radius);
+        this.model = new Circle(radius);
         
         // Setting values
-        this._location = location;
-        this._radius = radius;
+        this.location = location;
+        this.radius = radius;
         
         // Rotate 90 deg around x-axis so it is flat on the ground
-        this._model.setRotationAxis(Rotate.X_AXIS);
-        this._model.setRotate(90);
+        this.model.setRotationAxis(Rotate.X_AXIS);
+        this.model.setRotate(90);
         
         // Set color
-        this._model.setFill(Color.WHITE);
+        this.model.setFill(Color.WHITE);
         
         // Translate to location
-        this._model.setTranslateX(location.x);
-        this._model.setTranslateY(location.y);
-        this._model.setTranslateZ(location.z);
+        this.model.setTranslateX(location.x);
+        this.model.setTranslateY(location.y);
+        this.model.setTranslateZ(location.z);
     }
     
-    @Override
-    public void Destroy() {
-        
-        // Check if org.ssh.models is in the world
-        if (this.GetGame().GetWorldGroup().getChildren().contains(this._model)) {
-            
-            // Remove org.ssh.models from world
-            this.GetGame().GetWorldGroup().getChildren().remove(this._model);
-        }
-    }
-    
-    public Vector3f GetLocation() {
-        return this._location;
-    }
-    
-    public double GetRadius() {
-        return this._radius;
-    }
-    
+    /**
+     * Initialize method. Adds model to the world group.
+     */
     @Override
     public void Initialize() {
         
-        // Add org.ssh.models to the world group
-        this.GetGame().GetWorldGroup().getChildren().add(this._model);
+        // Add model to the world group
+        Platform.runLater(() -> this.GetGame().GetWorldGroup().getChildren().add(this.model));
     }
     
-    public void SetLocation(final Vector3f location) {
-        
-        this._location = location;
-        
-        this._model.setTranslateX(this._location.x);
-        this._model.setTranslateY(this._location.y);
-        this._model.setTranslateZ(this._location.z);
-    }
-    
-    public void SetRadius(final double radius) {
-        
-        this._radius = radius;
-        
-        this._model.setRadius(radius);
-    }
-    
+    /**
+     * Update method.
+     */
     @Override
     public void Update(final long timeDivNano) {
     }
+    
+    /**
+     * Destroy method. Removes model from the world
+     */
+    @Override
+    public void Destroy() {
+        
+        // Check if model in the world
+        if (this.GetGame().GetWorldGroup().getChildren().contains(this.model)) {
+            
+            // Remove org.ssh.models from world
+            this.GetGame().GetWorldGroup().getChildren().remove(this.model);
+        }
+    }
+    
+    /**
+     * Gets the location of the penalty spot
+     * 
+     * @return Returns a {@link Vector3f} containing the location of the penalty spot.
+     */
+    public Vector3f getLocation() {
+        
+        return this.location;
+    }
+    
+    /**
+     * Gets the radius of the penalty spot
+     * 
+     * @return Returns a double containing the radius of the penalty spot.
+     */
+    public double getRadius() {
+        
+        return this.radius;
+    }
+    
+    /**
+     * SetLocation method. This method sets the location of the model.
+     * 
+     * @param location The {@link Vector3f} of the location of the penalty spot.
+     */
+    public void setLocation(final Vector3f location) {
+        
+        // Setting location
+        this.location = location;
+        
+        // Translate model to location
+        this.model.setTranslateX(this.location.x);
+        this.model.setTranslateY(this.location.y);
+        this.model.setTranslateZ(this.location.z);
+    }
+    
+    /**
+     * SetRadius method. This method sets the radius of the model.
+     * 
+     * @param radius The radius of the model as double.
+     */
+    public void setRadius(final double radius) {
+        
+        // Setting radius
+        this.radius = radius;
+        
+        // Setting radius of the model
+        this.model.setRadius(radius);
+    }
+    
 }
