830ed1fe1717d53512cc5ec2b8a5cf1174ba8097
marklef2 <marklefering@gmail.com>
2015-11-08 15:16:14 +0100

    [0001 review-m] SkyboxGO, added javadoc, added comment, added Platform.runLater. Cleanup

--- src/org/ssh/field3d/gameobjects/SkyboxGO.java
+++ src/org/ssh/field3d/gameobjects/SkyboxGO.java
@@ -2,10 +2,13 @@ package org.ssh.field3d.gameobjects;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.ssh.field3d.core.game.Game;
 import org.ssh.field3d.core.gameobjects.GameObject;
+import org.ssh.util.Logger;
 
+import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -13,59 +16,111 @@ import javafx.scene.shape.CullFace;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 
-// TODO: Javadoc, cleanup & comment
+/**
+ * SkyboxGO class. This class is the class used for creating a 3d skybox. It creates an enormous
+ * {@link Sphere} and create a {@link PhongMaterial} from a file texture file. Face Culling is
+ * disabled so the texture is rendered on the inside to.
+ * 
+ * @see GameObject
+ *      
+ * @author marklef2
+ *         
+ */
 public class SkyboxGO extends GameObject {
     
-    private final Sphere        _model;
-    private final PhongMaterial _blueMaterial;
-                                
+    private static final Logger LOG                 = Logger.getLogger("SkyboxGO");
+    private static final String SKYBOX_TEXTURE_FILE = "./assets/textures/skybox.jpg";
+                                                    
+    private final Sphere        model;
+    private final PhongMaterial skyMaterial;
+      
+    
+    /**
+     * 
+     * Constructor
+     * 
+     * @param game
+     *            The {@link GameObject}'s {@link Game}.
+     */
     public SkyboxGO(final Game game) {
+        
+        // Initialize super class
         super(game);
         
+        // Define file input stream as null
         FileInputStream fs = null;
+        this.model = new Sphere(300000);
+        this.skyMaterial = new PhongMaterial(Color.WHITE);
         
+        // Try to load texture
         try {
             
             fs = new FileInputStream("./assets/textures/skybox.jpg");
             
         }
-        catch (final FileNotFoundException e) {
+        catch (final FileNotFoundException fileNotFoundException) {
             
-            // TODO: Logger handling
-            e.printStackTrace();
+            // TODO: Log exception
+            // Log error
+            LOG.info("Could not load: " + SKYBOX_TEXTURE_FILE);
+            LOG.finer(fileNotFoundException.getStackTrace().toString());
+            
+            return;
         }
         
-        this._model = new Sphere(300000);
+        // Setting diffuse color map
+        this.skyMaterial.setDiffuseMap(new Image(fs));
         
-        this._blueMaterial = new PhongMaterial(Color.WHITE);
-        this._blueMaterial.setDiffuseMap(new Image(fs));
+        // Setting model material
+        this.model.setMaterial(this.skyMaterial);
+        // Setting face culling to none
+        this.model.setCullFace(CullFace.NONE);
         
-        this._model.setMaterial(this._blueMaterial);
-        this._model.setCullFace(CullFace.NONE);
+        // Rotate 180 degrees around x-axis
+        this.model.setRotationAxis(Rotate.X_AXIS);
+        this.model.setRotate(180);
         
-        this._model.setRotationAxis(Rotate.X_AXIS);
-        this._model.setRotate(180);
-    }
-    
-    @Override
-    public void Destroy() {
-        
-        // Check if org.ssh.models is in the world group
-        if (this.GetGame().GetWorldGroup().getChildren().contains(this._model)) {
+        // Close file
+        try {
             
-            // Remove from world
-            this.GetGame().GetWorldGroup().getChildren().remove(this._model);
+            fs.close();
+        } catch (IOException ioException) {
+            
+            // TODO: Log exception
+            LOG.finer(ioException.getStackTrace().toString());
         }
     }
     
+    /**
+     * Initialize method. Adds models to the world.
+     */
     @Override
     public void Initialize() {
         
-        // Add org.ssh.models to the world group
-        this.GetGame().GetWorldGroup().getChildren().add(this._model);
+        // Add models to world group
+        Platform.runLater(() -> this.GetGame().GetWorldGroup().getChildren().add(this.model));
     }
     
+    /**
+     * Update method.
+     */
     @Override
     public void Update(final long timeDivNano) {
     }
+    
+    /**
+     * Destroy method. Removes model from world. 
+     */
+    @Override
+    public void Destroy() {
+        
+        // Check if model is in the world group
+        if (this.GetGame().GetWorldGroup().getChildren().contains(this.model)) {
+            
+            // Remove from world
+            this.GetGame().GetWorldGroup().getChildren().remove(this.model);
+        }
+    }
+    
+   
 }
