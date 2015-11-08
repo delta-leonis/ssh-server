39ca797cf2ea8598adb5ae7c2798fff7f9d995a5
marklef2 <marklefering@gmail.com>
2015-11-08 16:57:38 +0100

    [0008 review-m]CameraControlOverlayGO added javadoc, added comment, added todos, added runlater, did cleanup, var rename

--- src/org/ssh/field3d/gameobjects/overlay/CameraControlOverlayGO.java
+++ src/org/ssh/field3d/gameobjects/overlay/CameraControlOverlayGO.java
@@ -3,6 +3,7 @@ package org.ssh.field3d.gameobjects.overlay;
 import org.ssh.field3d.core.game.Game;
 import org.ssh.field3d.core.gameobjects.GameObject;
 
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
@@ -14,73 +15,169 @@ import javafx.scene.input.MouseEvent;
  *
  * This class is responsible for the 2d camera preset controls
  *
- * @author marklef2
- * @date 5-11-2015
+ * @author Mark Lefering
  */
+
+// TODO: fxml
 public class CameraControlOverlayGO extends GameObject {
     
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Preset button click events
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private static final int BUTTON_OFFSET = 35;
+                                           
+    private final Group      controlsGroup;
+    private final Button     buttonTopView;
+    private final Button     buttonSideView;
+    private final Button     button45DegView;
+    private final Button     buttonRotateLeft;
+    private final Button     buttonRotateRight;
+    private final Button     buttonRotateUp;
+    private final Button     buttonRotateDown;
+    private final Button     buttonZoomIn;
+    private final Button     buttonZoomOut;
+                             
     /**
+     * Constructor
      * 
-     * Button 45 degree org.ssh.view click event
-     * 
-     * @author marklef2
-     *        
+     * @param game
+     *            The {@link Game} of the {@link GameObject}.
      */
-    class OnButton45DegViewClicked implements EventHandler<MouseEvent> {
+    public CameraControlOverlayGO(final Game game) {
         
-        @Override
-        public void handle(final MouseEvent event) {
+        // Initialize super class
+        super(game);
+        
+        // Create group for the controls
+        this.controlsGroup = new Group();
+        
+        // Creating buttons
+        this.buttonTopView = new Button("Top View");
+        this.buttonSideView = new Button("Side View");
+        this.button45DegView = new Button("45 Degree View");
+        
+        this.buttonRotateLeft = new Button("Rotate Left");
+        this.buttonRotateRight = new Button("Rotate Right");
+        this.buttonRotateUp = new Button("Rotate Up");
+        this.buttonRotateDown = new Button("Rotate Down");
+        
+        this.buttonZoomIn = new Button("+");
+        this.buttonZoomOut = new Button("-");
+        
+        // Setting translation
+        this.buttonSideView.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET);
+        this.button45DegView.setTranslateY((2 * CameraControlOverlayGO.BUTTON_OFFSET));
+        
+        this.buttonRotateLeft.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 4);
+        this.buttonRotateRight.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 5);
+        this.buttonRotateUp.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 6);
+        this.buttonRotateDown.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 7);
+        
+        this.buttonZoomIn.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 9);
+        this.buttonZoomOut.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 10);
+        
+        // Adding buttons to controls group
+        this.controlsGroup.getChildren().add(this.button45DegView);
+        this.controlsGroup.getChildren().add(this.buttonSideView);
+        this.controlsGroup.getChildren().add(this.buttonTopView);
+        
+        this.controlsGroup.getChildren().add(this.buttonRotateLeft);
+        this.controlsGroup.getChildren().add(this.buttonRotateRight);
+        this.controlsGroup.getChildren().add(this.buttonRotateUp);
+        this.controlsGroup.getChildren().add(this.buttonRotateDown);
+        
+        this.controlsGroup.getChildren().add(this.buttonZoomIn);
+        this.controlsGroup.getChildren().add(this.buttonZoomOut);
+    }
+    
+    /**
+     * Initialize method. This method adds the control group to the 2d group and hooks events.
+     */
+    @Override
+    public void Initialize() {
+        
+        // Adding control group to 2d group
+        Platform.runLater(() -> this.GetGame().Get2DGroup().getChildren().add(this.controlsGroup));
+        
+        // Hook events
+        this.button45DegView.setOnMouseClicked(new OnButton45DegViewClicked());
+        this.buttonSideView.setOnMouseClicked(new OnButtonSideViewClicked());
+        this.buttonTopView.setOnMouseClicked(new OnButtonTopViewClicked());
+        
+        this.buttonRotateLeft.setOnMouseClicked(new OnButtonRotateLeftClicked());
+        this.buttonRotateRight.setOnMouseClicked(new OnButtonRotateRightClicked());
+        this.buttonRotateUp.setOnMouseClicked(new OnButtonRotateUpClicked());
+        this.buttonRotateDown.setOnMouseClicked(new OnButtonRotateDownClicked());
+        
+        this.buttonZoomIn.setOnMouseClicked(new OnButtonZoomInClicked());
+        this.buttonZoomOut.setOnMouseClicked(new OnButtonZoomOutClicked());
+    }
+    
+    /**
+     * Destroy method. This method removes the control group from the 2d group
+     */
+    @Override
+    public void Destroy() {
+        
+        // TODO: remove event handlers
+        
+        // If 2d group contains the controls group
+        if (this.GetGame().Get2DGroup().getChildren().contains(this.controlsGroup)) {
             
-            // TODO Set camera to 45 degree angle, set location to center, set zoom
-            CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateY(0);
-            CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateX(45);
-            
+            // Remove from 2d group
+            Platform.runLater(() -> this.GetGame().Get2DGroup().getChildren().remove(this.controlsGroup));
         }
         
+        // Unhook events
+        this.button45DegView.setOnMouseClicked(null);
+        this.buttonSideView.setOnMouseClicked(null);
+        this.buttonTopView.setOnMouseClicked(null);
+        
+        this.buttonRotateLeft.setOnMouseClicked(null);
+        this.buttonRotateRight.setOnMouseClicked(null);
+        this.buttonRotateUp.setOnMouseClicked(null);
+        this.buttonRotateDown.setOnMouseClicked(null);
+        
+        this.buttonZoomIn.setOnMouseClicked(null);
+        this.buttonZoomOut.setOnMouseClicked(null);
+    }
+    
+    /**
+     * 
+     * @param timeDivNano
+     *            The time difference in nanoseconds.
+     */
+    @Override
+    public void Update(final long timeDivNano) {
     }
     
     /**
      * 
      * Button rotate down click event.
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
      */
     class OnButtonRotateDownClicked implements EventHandler<MouseEvent> {
         
         @Override
-        public void handle(final MouseEvent event) {
+        public void handle(final MouseEvent mouseEvent) {
             
-            // TODO Smooth out rotation
+            // TODO: Smooth out rotation
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera()
                     .SetRotateX(CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().GetRotateX() - 10);
         }
         
     }
     
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Rotation button click events
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     /**
      * 
      * Button rotate left click event.
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
      */
     class OnButtonRotateLeftClicked implements EventHandler<MouseEvent> {
         
         @Override
-        public void handle(final MouseEvent event) {
+        public void handle(final MouseEvent mouseEvent) {
             
-            // TODO Smooth out rotation
+            // TODO: Smooth out rotation
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera()
                     .SetRotateY(CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().GetRotateY() - 10);
         }
@@ -90,13 +187,12 @@ public class CameraControlOverlayGO extends GameObject {
      * 
      * Button rotate right click event.
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
      */
     class OnButtonRotateRightClicked implements EventHandler<MouseEvent> {
         
         @Override
-        public void handle(final MouseEvent event) {
+        public void handle(final MouseEvent mouseEvent) {
             
             // TODO Smooth out rotation
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera()
@@ -109,15 +205,14 @@ public class CameraControlOverlayGO extends GameObject {
      * 
      * Button rotate up click event.
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
      */
     class OnButtonRotateUpClicked implements EventHandler<MouseEvent> {
         
         @Override
-        public void handle(final MouseEvent event) {
+        public void handle(final MouseEvent mouseEvent) {
             
-            // TODO Smooth out rotation
+            // TODO: Smooth out rotation
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera()
                     .SetRotateX(CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().GetRotateX() + 10);
         }
@@ -126,17 +221,17 @@ public class CameraControlOverlayGO extends GameObject {
     
     /**
      * 
-     * Button side org.ssh.view click event
+     * Button side view click event
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
      */
     class OnButtonSideViewClicked implements EventHandler<MouseEvent> {
         
         @Override
-        public void handle(final MouseEvent event) {
+        public void handle(final MouseEvent mouseEvent) {
             
-            // TODO Set camera to side org.ssh.view, set location to center, set zoom
+            // TODO: Set location to center
+            // TODO: Set zoom
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateY(0);
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateX(0);
             
@@ -146,168 +241,70 @@ public class CameraControlOverlayGO extends GameObject {
     
     /**
      * 
-     * Button top org.ssh.view click event.
+     * Button 45 degree view click event
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
+     */
+    class OnButton45DegViewClicked implements EventHandler<MouseEvent> {
+        
+        @Override
+        public void handle(final MouseEvent mouseEvent) {
+            
+            // TODO: Set location to center
+            // TODO: Set zoom
+            CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateY(0);
+            CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateX(45);
+        }
+    }
+    
+    /**
+     * 
+     * Button view click event.
+     * 
+     * @author Mark Lefering
+     *         
      */
     class OnButtonTopViewClicked implements EventHandler<MouseEvent> {
         
         @Override
         public void handle(final MouseEvent event) {
             
-            // TODO Set camera to top org.ssh.view, set location to center, set zoom
+            // TODO: Set location to center
+            // TODO: Set zoom
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateY(0);
             CameraControlOverlayGO.this.GetGame().GetThirdPersonCamera().SetRotateX(90);
-            
         }
         
     }
     
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Zoom button click events
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     /**
      * 
      * Button zoom in click event.
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
      */
     class OnButtonZoomInClicked implements EventHandler<MouseEvent> {
         
         @Override
         public void handle(final MouseEvent event) {
             
-            // TODO zoom in
-            // GetGame().GetThirdPersonCamera().SetRotateY(0);
-            // GetGame().GetThirdPersonCamera().SetRotateX(90);
-            
+            // TODO: zoom in
         }
-        
     }
     
     /**
      * 
      * Button zoom out click event.
      * 
-     * @author marklef2
-     *        
+     * @author Mark Lefering
+     *         
      */
     class OnButtonZoomOutClicked implements EventHandler<MouseEvent> {
         
         @Override
         public void handle(final MouseEvent event) {
             
-            // TODO Zoom out
+            // TODO: Zoom out
         }
-    }
-    
-    // TODO: move to config
-    private static final int BUTTON_OFFSET = 35;
-    private final Group      _controlsGroup;
-                             
-    private final Button     _buttonTopView;
-                             
-    private final Button     _buttonSideView;
-                             
-    private final Button     _button45DegView;
-                             
-    private final Button     _buttonRotateLeft;
-                             
-    private final Button     _buttonRotateRight;
-                             
-    private final Button     _buttonRotateUp;
-                             
-    private final Button     _buttonRotateDown;
-                             
-    private final Button     _buttonZoomIn;
-                             
-    private final Button     _buttonZoomOut;
-                             
-    public CameraControlOverlayGO(final Game game) {
-        
-        // Initialize super class
-        super(game);
-        
-        // Create group for the controls
-        this._controlsGroup = new Group();
-        
-        // Creating buttons
-        this._buttonTopView = new Button("Top View");
-        this._buttonSideView = new Button("Side View");
-        this._button45DegView = new Button("45 Degree View");
-        
-        this._buttonRotateLeft = new Button("Rotate Left");
-        this._buttonRotateRight = new Button("Rotate Right");
-        this._buttonRotateUp = new Button("Rotate Up");
-        this._buttonRotateDown = new Button("Rotate Down");
-        
-        this._buttonZoomIn = new Button("+");
-        this._buttonZoomOut = new Button("-");
-        
-        // Setting translation
-        this._buttonSideView.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET);
-        this._button45DegView.setTranslateY((2 * CameraControlOverlayGO.BUTTON_OFFSET));
-        
-        this._buttonRotateLeft.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 4);
-        this._buttonRotateRight.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 5);
-        this._buttonRotateUp.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 6);
-        this._buttonRotateDown.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 7);
-        
-        this._buttonZoomIn.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 9);
-        this._buttonZoomOut.setTranslateY(CameraControlOverlayGO.BUTTON_OFFSET * 10);
-        
-        // Adding buttons to controls group
-        this._controlsGroup.getChildren().add(this._button45DegView);
-        this._controlsGroup.getChildren().add(this._buttonSideView);
-        this._controlsGroup.getChildren().add(this._buttonTopView);
-        
-        this._controlsGroup.getChildren().add(this._buttonRotateLeft);
-        this._controlsGroup.getChildren().add(this._buttonRotateRight);
-        this._controlsGroup.getChildren().add(this._buttonRotateUp);
-        this._controlsGroup.getChildren().add(this._buttonRotateDown);
-        
-        this._controlsGroup.getChildren().add(this._buttonZoomIn);
-        this._controlsGroup.getChildren().add(this._buttonZoomOut);
-    }
-    
-    @Override
-    public void Destroy() {
-        
-        // TODO remove event handlers
-        
-        // If 2d group contains the controls group
-        if (this.GetGame().Get2DGroup().getChildren().contains(this._controlsGroup)) {
-            
-            // Remove from 2d group
-            this.GetGame().Get2DGroup().getChildren().remove(this._controlsGroup);
-        }
-    }
-    
-    @Override
-    public void Initialize() {
-        
-        // Adding buttons to 2d group
-        this.GetGame().Get2DGroup().getChildren().add(this._controlsGroup);
-        
-        // Hook events
-        this._button45DegView.setOnMouseClicked(new OnButton45DegViewClicked());
-        this._buttonSideView.setOnMouseClicked(new OnButtonSideViewClicked());
-        this._buttonTopView.setOnMouseClicked(new OnButtonTopViewClicked());
-        
-        this._buttonRotateLeft.setOnMouseClicked(new OnButtonRotateLeftClicked());
-        this._buttonRotateRight.setOnMouseClicked(new OnButtonRotateRightClicked());
-        this._buttonRotateUp.setOnMouseClicked(new OnButtonRotateUpClicked());
-        this._buttonRotateDown.setOnMouseClicked(new OnButtonRotateDownClicked());
-        
-        this._buttonZoomIn.setOnMouseClicked(new OnButtonZoomInClicked());
-        this._buttonZoomOut.setOnMouseClicked(new OnButtonZoomOutClicked());
-    }
-    
-    @Override
-    public void Update(final long timeDivNano) {
     }
 }
