56cf55e8a58c0896b36139d13a4d4d86740f931d
marklef2 <marklefering@gmail.com>
2015-11-09 14:09:00 +0100

    [0025 review-m] FieldGame updated javadoc, renamed vars, renamed methods

--- src/org/ssh/field3d/FieldGame.java
+++ src/org/ssh/field3d/FieldGame.java
@@ -13,55 +13,79 @@ import javafx.scene.PointLight;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.paint.Color;
 
+// TODO: Auto-generated Javadoc
 /**
  *
  * FieldGame class This class is the main part of the 3d field, from here everything in the 3d world
  * is created & managed.
  *
  * @author Mark Lefering - 330430
- *        
+ *         
  */
 public class FieldGame extends Game {
     
-    // TODO: move to config file, or get from org.ssh.models
+    // TODO: move to config file, or get from the vision model
+    /** The Constant FIELD_TILE_WIDTH. */
     public static final double           FIELD_TILE_WIDTH      = 500.0;
+                                                               
+    /** The Constant FIELD_TILE_DEPTH. */
     public static final double           FIELD_TILE_DEPTH      = 500.0;
+                                                               
+    /** The Constant FIELD_WIDTH. */
     public static final double           FIELD_WIDTH           = 9000.0;
+                                                               
+    /** The Constant FIELD_DEPTH. */
     public static final double           FIELD_DEPTH           = 6000.0;
+                                                               
+    /** The Constant FIELD_REAL_WIDTH. */
     public static final double           FIELD_REAL_WIDTH      = FieldGame.FIELD_WIDTH
             + (2.0 * FieldGame.FIELD_TILE_WIDTH);
+            
+    /** The Constant FIELD_REAL_DEPTH. */
     public static final double           FIELD_REAL_DEPTH      = FieldGame.FIELD_DEPTH
             + (2.0 * FieldGame.FIELD_TILE_DEPTH);
             
+    /** The Constant FIELD_LINE_HEIGHT. */
     public static final double           FIELD_LINE_HEIGHT     = 5.0;
+                                                               
+    /** The Constant FIELD_LINE_WIDTH. */
     public static final double           FIELD_LINE_WIDTH      = 10.0f;
+                                                               
+    /** The Constant FIELD_HEIGHT. */
     public static final double           FIELD_HEIGHT          = 1.0;
+                                                               
+    /** The Constant FIELD_GOAL_WIDTH. */
     public static final double           FIELD_GOAL_WIDTH      = 1000.0;
+                                                               
+    /** The Constant FIELD_GOAL_DEPTH. */
     public static final double           FIELD_GOAL_DEPTH      = 200.0;
+                                                               
+    /** The Constant FIELD_GOAL_HEIGHT. */
     public static final double           FIELD_GOAL_HEIGHT     = 190.0;
+                                                               
+    /** The Constant FIELD_GOAL_LINE_WIDTH. */
     public static final double           FIELD_GOAL_LINE_WIDTH = 5.0;
                                                                
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Private variables
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    private final AmbientLight           _ambientLight;
-    private final PointLight             _pointLight1, _pointLight2, _pointLight3, _pointLight4;
+    /** The ambient light. */
+    private final AmbientLight           ambientLight;
                                          
-    private final FieldGO                _field;
-    private final CameraControlOverlayGO _cameraControlOverlay;
-    private final CarGO                  _easterCar;
+    /** The west point lights */
+    private final PointLight             pointLightWestSouth, pointLightWestNorth;
+    /** The east point lights */
+    private final PointLight             pointLightEastSouth, pointLightEastNorth;
                                          
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Constructors
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /** The field game object */
+    private final FieldGO                fieldGO;
+                                         
+    /** The camera control overlay game object. */
+    private final CameraControlOverlayGO cameraControlOverlayGO;
+                                         
+    /** The easter car game object */
+    private final CarGO                  easterCarGO;
+                                         
     /**
-     * 
-     * Constructor
-     * 
+     * Constructor.
+     *
      * @param root
      *            The root of the SubScene.
      * @param width
@@ -77,37 +101,37 @@ public class FieldGame extends Game {
         super(root, width, height, true, antiAliasing);
         
         // Creating ambient light
-        this._ambientLight = new AmbientLight(Color.DARKGRAY);
+        this.ambientLight = new AmbientLight(Color.DARKGRAY);
         
         // Creating point lights
-        this._pointLight1 = new PointLight(Color.WHITE);
-        this._pointLight2 = new PointLight(Color.WHITE);
-        this._pointLight3 = new PointLight(Color.WHITE);
-        this._pointLight4 = new PointLight(Color.WHITE);
+        this.pointLightWestSouth = new PointLight(Color.WHITE);
+        this.pointLightWestNorth = new PointLight(Color.WHITE);
+        this.pointLightEastSouth = new PointLight(Color.WHITE);
+        this.pointLightEastNorth = new PointLight(Color.WHITE);
         
         // Creating field GameObject
-        this._field = new FieldGO(this, FieldGame.FIELD_REAL_WIDTH, FieldGame.FIELD_REAL_DEPTH);
+        this.fieldGO = new FieldGO(this, FieldGame.FIELD_REAL_WIDTH, FieldGame.FIELD_REAL_DEPTH);
         // Creating camera control overlay GameObject
-        this._cameraControlOverlay = new CameraControlOverlayGO(this);
+        this.cameraControlOverlayGO = new CameraControlOverlayGO(this);
         // Creating easter egg car GameObject
-        this._easterCar = new CarGO(this);
+        this.easterCarGO = new CarGO(this);
         
         // Setup lights
-        this._pointLight1.setTranslateX(-(FieldGame.FIELD_WIDTH / 4.0));
-        this._pointLight1.setTranslateY(2000);
-        this._pointLight1.setTranslateZ(-(FieldGame.FIELD_DEPTH / 4.0));
+        this.pointLightWestSouth.setTranslateX(-(FieldGame.FIELD_WIDTH / 4.0));
+        this.pointLightWestSouth.setTranslateY(2000);
+        this.pointLightWestSouth.setTranslateZ(-(FieldGame.FIELD_DEPTH / 4.0));
         
-        this._pointLight2.setTranslateX(-(FieldGame.FIELD_WIDTH / 4.0));
-        this._pointLight2.setTranslateY(2000);
-        this._pointLight2.setTranslateZ(FieldGame.FIELD_DEPTH / 4.0);
+        this.pointLightWestNorth.setTranslateX(-(FieldGame.FIELD_WIDTH / 4.0));
+        this.pointLightWestNorth.setTranslateY(2000);
+        this.pointLightWestNorth.setTranslateZ(FieldGame.FIELD_DEPTH / 4.0);
         
-        this._pointLight3.setTranslateX(FieldGame.FIELD_WIDTH / 4.0);
-        this._pointLight3.setTranslateY(2000);
-        this._pointLight3.setTranslateZ(-(FieldGame.FIELD_DEPTH / 4.0));
+        this.pointLightEastSouth.setTranslateX(FieldGame.FIELD_WIDTH / 4.0);
+        this.pointLightEastSouth.setTranslateY(2000);
+        this.pointLightEastSouth.setTranslateZ(-(FieldGame.FIELD_DEPTH / 4.0));
         
-        this._pointLight4.setTranslateX(FieldGame.FIELD_WIDTH / 4.0);
-        this._pointLight4.setTranslateY(2000);
-        this._pointLight4.setTranslateZ(FieldGame.FIELD_DEPTH / 4.0);
+        this.pointLightEastNorth.setTranslateX(FieldGame.FIELD_WIDTH / 4.0);
+        this.pointLightEastNorth.setTranslateY(2000);
+        this.pointLightEastNorth.setTranslateZ(FieldGame.FIELD_DEPTH / 4.0);
         
         // Set minimal mouse wheel value
         this.getMouseInputHandler().setMinMouseWheelValue(-1000);
@@ -119,23 +143,67 @@ public class FieldGame extends Game {
     }
     
     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize() {
+        
+        // Add lights to the world
+        this.getWorldGroup().getChildren().add(this.ambientLight);
+        this.getWorldGroup().getChildren().add(this.pointLightWestSouth);
+        this.getWorldGroup().getChildren().add(this.pointLightWestNorth);
+        this.getWorldGroup().getChildren().add(this.pointLightEastSouth);
+        this.getWorldGroup().getChildren().add(this.pointLightEastNorth);
+        
+        // Adding game objects
+        this.addGameObject(this.fieldGO);
+        this.addGameObject(this.cameraControlOverlayGO);
+        this.addGameObject(this.easterCarGO);
+        
+        // Create some robots
+        this.createRobots();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update(final long timeDivNano) {
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void destroy() {
+    }
+    
+    /**
      * 
      * AddRobot method This method adds a RobotGO to the game.
      * 
      * @param robot
      *            The RobotGO to add to the game.
      */
-    public void AddRobot(final RobotGO robot) {
+    public void addRobot(final RobotGO robot) {
         
         // Add robot to game
         this.addGameObject(robot);
     }
     
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Private methods
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     * 
+     * RemoveRobot method This method removes a RobotGO from the game.
+     * 
+     * @param robot
+     *            The RobotGO to remove from the game.
+     */
+    public void removeRobot(final RobotGO robot) {
+        
+        // Remove robot from game
+        this.removeGameObject(robot);
+    }
+    
     /**
      * createRobots method This method creates some robots.
      */
@@ -152,50 +220,5 @@ public class FieldGame extends Game {
             // Add robot to game objects
             this.addGameObject(robot);
         }
-    }
-    
-    @Override
-    public void destroy() {
-    }
-    
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    //
-    // Abstract methods from GameObject
-    //
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    @Override
-    public void initialize() {
-        
-        // Add lights to the world
-        this.getWorldGroup().getChildren().add(this._ambientLight);
-        this.getWorldGroup().getChildren().add(this._pointLight1);
-        this.getWorldGroup().getChildren().add(this._pointLight2);
-        this.getWorldGroup().getChildren().add(this._pointLight3);
-        this.getWorldGroup().getChildren().add(this._pointLight4);
-        
-        // Adding game objects
-        this.addGameObject(this._field);
-        this.addGameObject(this._cameraControlOverlay);
-        this.addGameObject(this._easterCar);
-        
-        // Create some robots
-        this.createRobots();
-    }
-    
-    /**
-     * 
-     * RemoveRobot method This method removes a RobotGO from the game.
-     * 
-     * @param robot
-     *            The RobotGO to remove from the game.
-     */
-    public void RemoveRobot(final RobotGO robot) {
-        
-        // Remove robot from game
-        this.removeGameObject(robot);
-    }
-    
-    @Override
-    public void update(final long timeDivNano) {
     }
 }
