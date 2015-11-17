677577ebe62b99fa5020a717bf0029731272c3c5
Thoma <ThomasHakkers@hotmail.com>
2015-11-17 11:57:53 +0100

    Cleaned up CustomDebugLib

--- src/org/ssh/ui/lua/console/CustomDebugLib.java
+++ src/org/ssh/ui/lua/console/CustomDebugLib.java
@@ -1,26 +1,40 @@
 package org.ssh.ui.lua.console;
 
-import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.DebugLib;
 
+/**
+ * A custom class that extends {@link DebugLib} to be able to interrupt threads in lua
+ * @author Thomas Hakkers
+ * Credits go to this guy: http://stackoverflow.com/questions/17496868/lua-java-luaj-handling-or-interrupting-infinite-loops-and-threads
+ */
 public class CustomDebugLib extends DebugLib {
     private boolean interrupted = false;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onInstruction(int pc, Varargs v, int top) {
-        System.out.println("On instruction" + interrupted);
         if (interrupted) {
-            System.out.println("WOW HET WERKT");
             interrupted = false;
             throw new ScriptInterruptException();
         }
         super.onInstruction(pc, v, top);
     }
     
+    /**
+     * Interrupts the current thread
+     */
     public void interrupt(){
         interrupted = true;
     }
 
-    public static class ScriptInterruptException extends RuntimeException {}
+    /**
+     * The exception that gets thrown when a lua thread is interrupted
+     */
+    @SuppressWarnings ("serial")
+    public static class ScriptInterruptException extends RuntimeException {
+        
+    }
 }
