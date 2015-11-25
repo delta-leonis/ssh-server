50a9b9e6f89ef8e8789faac058602b22262a98f6
Thoma <ThomasHakkers@hotmail.com>
2015-11-25 17:37:25 +0100

    Removed busy wait

--- lua.d/init.lua
+++ lua.d/init.lua
@@ -9,13 +9,8 @@ function newPoint(x, y)
 end
 
 --[[ sleep function ]]-- 
---[[ I'm sorry for the busy wait http://lua-users.org/wiki/SleepFunction ]]--
-local clock = os.clock
 function sleep(n)
-    local t0 = clock() * 1000
-    while clock() * 1000 - t0 <= n do 
-    -- Do nothing
-    end 
+	luajava.bindClass("java.lang.Thread"):currentThread():sleep(n)
 end
 
 -- Create "aliases" for all robots. Robot B0 is robotB[0]
