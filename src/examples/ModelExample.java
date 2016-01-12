656c9d980bea50c99428193d4b38c4ded59b2cfb
Jeroen de Jong <thumbnail95@gmail.com>
2016-01-12 02:03:30 +0100

    minor removal

--- src/examples/ModelExample.java
+++ src/examples/ModelExample.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 public class ModelExample {
 
-    private static Logger LOG = Logger.getLogger();
+    private static final Logger LOG = Logger.getLogger();
 
     private ModelExample() {
     }
