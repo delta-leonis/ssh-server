dee13dbada69898b839119721ddc9495532a0b4a
Jeroen de Jong <beheer@jeroendj.nl>
2016-01-13 13:03:39 +0100

    added javadoc

--- src/org/ssh/managers/manager/UI.java
+++ src/org/ssh/managers/manager/UI.java
@@ -71,7 +71,7 @@ public final class UI implements ManagerInterface<UIComponent<? extends Pane>> {
     /**
      * Returns an Optional containing the window with the specified name.
      *
-     * @param <P>  the generic type
+     * @param <U>  the generic type
      * @param name The name of the requested window.
      * @return An Optional containing the window.
      */
@@ -102,10 +102,20 @@ public final class UI implements ManagerInterface<UIComponent<? extends Pane>> {
                 .findAny();
     }
 
+    /**
+     * Adds a component to the list of components
+     * @param component component to add
+     */
     public static void add(final UIComponent<?> component) {
         controller.add(component);
     }
 
+    /**
+     * Find a compontent based on (a part of) it's name
+     * @param pattern pattern to match
+     * @param <N> return type
+     * @return list of found components
+     */
     public <N extends UIComponent<?>> List<N> find(String pattern) {
         return controller.find(pattern);
     }
@@ -114,50 +124,100 @@ public final class UI implements ManagerInterface<UIComponent<? extends Pane>> {
 //        return controller.get(name);
 //    }
 
+    /**
+     * @param <N> preferred return type
+     * @return all current managed UIComponents
+     */
     public <N extends AbstractManageable> List<N> getAll() {
         return controller.getAll();
     }
 
+    /**
+     * Returns the highest node that's a parent
+     * @param child child to find highest parent node for
+     * @return the highest node that's a parent
+     */
     public static Parent getHighestParent(Parent child) {
         if (child.getParent() == null)
+            // this is it
             return child;
+        // let's check it's parent
         return UI.getHighestParent(child.getParent());
     }
 
+    /**
+     * All Nodes of a certain node and recursively all children
+     * @param root rootnode to start
+     * @return All Nodes of a certain node and recursively all children
+     */
     public static List<Node> getAllNodes(Parent root) {
+        //create a list once
         ArrayList<Node> nodes = new ArrayList<Node>();
+        //get all nodes, and dump them in the provided list
         addAllDescendents(root, nodes);
+        //return the nodes
         return nodes;
     }
 
+    /**
+     * recursive method for finding al nodes contained by a node's children
+     * @param parent node with children
+     * @param nodes list of nodes to add children to
+     */
     private static void addAllDescendents(Parent parent, ArrayList<Node> nodes) {
+        // loop all children
         for (Node node : parent.getChildrenUnmodifiable()) {
+            //put them in a map
             nodes.add(node);
+            // if this node has children, add those as well
             if (node instanceof Parent)
                 UI.addAllDescendents((Parent) node, nodes);
         }
     }
 
+    /**
+     * Adds a widget to the manager
+     * @param widget widget to add to the manager
+     */
     public static void addWidget(AbstractWidget widget) {
         controller.addWidget(widget);
     }
 
+    /**
+     * @return a list in the right order for the shortcuts
+     */
     public static List<AbstractWidget> getOrderedWidgetsForShortcuts() {
         return controller.getOrderedShortcuts();
     }
 
+    /**
+     * @return a list in the right order for the widget window
+     */
     public static List<AbstractWidget> getOrderedWidgetsForWindow() {
         return controller.getOrderedWidgetsForWindow();
     }
 
+    /**
+     * Swap a widget's location
+     * @param row1 pos to swap to
+     * @param row2 pos to swap from
+     */
     public static void swapWindowOrder(int row1, int row2) {
         controller.swapWindowOrder(row1, row2);
     }
 
+    /**
+     * Swap a widget's location
+     * @param col1 pos to swap to
+     * @param col2 pos to swap from
+     */
     public static void swapShortcutOrder(int col1, int col2) {
         controller.swapShortcutOrder(col1, col2);
     }
 
+    /**
+     * @return total number of widgets
+     */
     public static int getNumberOfWidgets() {
         return controller.getNumberOfWidgets();
     }
@@ -178,10 +238,19 @@ public final class UI implements ManagerInterface<UIComponent<? extends Pane>> {
         return UI.uiControllers.add(new MainWindow("Leo regulus", primaryStage));
     }
 
+    /**
+     * Flip a {@link ImageView image} 180 degrees
+     * @param image image to flip
+     */
     public static void flipImage(ImageView image) {
         image.setRotate((image.getRotate() + 180) % 360);
     }
 
+    /**
+     * Binds the height and width of a node
+     * @param child child to bind from
+     * @param parent child to bind to
+     */
     public static final void bindSize(Node child, Node parent) {
         if (!(child instanceof Region || child instanceof SubScene)) {
             UI.LOG.info("Could not bind child, since it isn't an instance of Region or SubScene");
@@ -205,16 +274,31 @@ public final class UI implements ManagerInterface<UIComponent<? extends Pane>> {
             bindSize((Region) child, (SubScene) parent);
     }
 
+    /**
+     * Specifically bind a SubScene to a Region
+     * @param child child to bind from
+     * @param parent child to bind to
+     */
     private static final void bindSize(SubScene child, Region parent) {
         child.heightProperty().bind(parent.heightProperty());
         child.widthProperty().bind(parent.widthProperty());
     }
 
+    /**
+     * Specifically bind a SubScene to a SubScene
+     * @param child child to bind from
+     * @param parent child to bind to
+     */
     private static final void bindSize(SubScene child, SubScene parent) {
         child.heightProperty().bind(parent.heightProperty());
         child.widthProperty().bind(parent.widthProperty());
     }
 
+    /**
+     * Specifically bind a Region to a SubScene
+     * @param child child to bind from
+     * @param parent child to bind to
+     */
     public static final void bindSize(Region child, SubScene parent) {
         child.maxHeightProperty().bind(parent.heightProperty());
         child.minHeightProperty().bind(parent.heightProperty());
@@ -222,6 +306,11 @@ public final class UI implements ManagerInterface<UIComponent<? extends Pane>> {
         child.minWidthProperty().bind(parent.widthProperty());
     }
 
+    /**
+     * Specifically bind a Region to a Region
+     * @param child child to bind from
+     * @param parent child to bind to
+     */
     public static final void bindSize(Region child, Region parent) {
         child.maxHeightProperty().bind(parent.heightProperty());
         child.minHeightProperty().bind(parent.heightProperty());
--- src/org/ssh/ui/UIComponent.java
+++ src/org/ssh/ui/UIComponent.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.SubScene;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
@@ -15,48 +16,81 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 
 /**
+ * Containerclass for a {@link java.awt.Component} of type {@link Pane}. This way the UI components are able to be managed
+ * by a coordinating class (in this case {@link UI})
+ *
  * @author Jeroen de Jong
  * @date 12/19/2015
  */
 public class UIComponent<N extends Pane> extends AbstractManageable {
 
+    /** component to manage */
     private N component;
 
+    /**
+     * Instantiates a new UIComponent
+     * @param name unique name of this component
+     * @param fxmlFile file that describes the structure of this component
+     */
     public UIComponent(String name, String fxmlFile) {
         super(name);
 
+        //auto assign to the manager
         UI.add(this);
 
+        //load the fxml
         loadFXML(fxmlFile);
 
     }
 
+    /**
+     * @return current stage of this component
+     */
     @Nullable
     public Stage getStage() {
         return (Stage) getComponent().getScene().getWindow();
     }
 
+    /**
+     * @return The containing component for actual use in GUI
+     */
     @Nullable
     public N getComponent() {
         return component;
     }
 
+    /**
+     * Add a component to this pane
+     * @param component component to add
+     * @param <M> Type of component
+     * @return success value, true if added successfully
+     */
     public <M extends Node> boolean add(M component) {
         return add(component, false);
     }
 
+    /**
+     * Adds a component at a specific place in the GUI {@see fx:id}
+     * @param component component to add
+     * @param identifier identifier to place the component in or at
+     * @param bind whether to bind the size to the parent
+     * @param <M> type of component
+     * @return success value, true if added succesfully
+     */
     public <M extends Node> boolean add(M component, String identifier, boolean bind) {
         if (this.getComponent().getChildren().contains(component)) {
             UIComponent.LOG.info("Could not add %s, since it already has been added", component);
             return false;
         }
 
+        // find the node based on the identifier
         Node selected = this.getComponent().lookup(identifier);
         if (selected == null) {
             UIComponent.LOG.info("Could not find %s, thus element has not been added.", identifier);
             return false;
         }
 
+        //check how it can be added
         if (selected instanceof Pane)
             ((Pane) selected).getChildren().add(component);
         else if (selected instanceof Group)
@@ -66,47 +100,89 @@ public class UIComponent<N extends Pane> extends AbstractManageable {
             return false;
         }
 
+        //bind if necessary
         if (bind)
             UI.bindSize(component, selected);
 
+        // it has been added
         return true;
     }
 
+    /**
+     * Add a component at a certain index. This may be useful for use in {@link FlowPane}s
+     * @param component component to add
+     * @param index index at which the specified element is to be inserted
+     * @param bind whether to bind the size to the parent
+     * @param <M> type of component
+     * @return success value, true if successful
+     */
     public <M extends Node> boolean add(M component, int index, boolean bind) {
+        // should be unique
         if (this.getComponent().getChildren().contains(component)) {
             UIComponent.LOG.info("Could not add %s, since it already has been added", component);
             return false;
         }
 
+        // add at the given index
         this.getComponent().getChildren().add(index, component);
 
+        //bind if necessary
         if (bind)
             UI.bindSize(component, this.component);
 
+        // successful add
         return true;
 
     }
 
+    /**
+     * Add component to a Pane
+     * @param component component to add
+     * @param bind whether to bind the size to the parent
+     * @param <M> type of component
+     * @return success value, true if successful
+     */
     public <M extends Node> boolean add(M component, boolean bind) {
         return add(component, getComponent().getChildren().size() - 1, bind);
     }
 
-    public <M extends Node> void bindSize(M component) {
-        if (component instanceof Region)
-            UI.bindSize((Region) component, this.component);
-        else if (component instanceof SubScene)
-            this.bindSubSceneSize((SubScene) component);
+    /**
+     * binds height and width to this Pane
+     * @param child child to bind from
+     * @param <M> type of component
+     */
+    public <M extends Node> void bindSize(M child) {
+        if (child instanceof Region)
+            UI.bindSize((Region) child, this.component);
+        else if (child instanceof SubScene)
+            this.bindSubSceneSize((SubScene) child);
     }
 
+    /**
+     * Adds a UIComponent to this component
+     * @param childComponent UIComponent to add
+     * @return success value
+     */
     public boolean add(UIComponent childComponent) {
         return add(childComponent, false);
     }
 
+    /**
+     * Adds a UIComponent to this component
+     * @param childComponent UIComponent to add
+     * @param bind whether to bind the height and width
+     * @return success value
+     */
     public boolean add(UIComponent childComponent, boolean bind) {
         return add(childComponent.getComponent(), bind);
     }
 
 
+    /**
+     * Specific implementation how to bind height and width to a SubScene
+     * @param childComponent childComponent to bind from
+     * @param <M> type of component
+     */
     public <M extends SubScene> void bindSubSceneSize(M childComponent) {
         childComponent.heightProperty().bind(component.heightProperty());
         childComponent.widthProperty().bind(component.widthProperty());
